d205f9c05ac7ce89f69d50e9351a6ae33b1bb8b1
손종효 <jbson1998@naver.com>
2023-03-08 15:42:25 +0900

    feat:bar create
    topbar와 footer 생성
    
    Issue : S08P22B208-104

--- masida/src/components/Footer/Footer.tsx
+++ masida/src/components/Footer/Footer.tsx
@@ -1,7 +1,21 @@
+import style from "./Footer.module.scss";
+
 const Footer = () => {
   return (
     <>
-      <h1>나는 하단바야 ㅋㅋ</h1>
+      <div className={style.main}>
+        <div className={style.main_left}>
+          <div>
+            <span>회사소개</span>
+            <span>이용약관</span>
+            <span>개인정보처리방침</span>
+          </div>
+          <div className={style.main_left_descrption}>
+            © SSAFY Corp. Masida. All rights Reserved
+          </div>
+        </div>
+        <div className={style.main_right}></div>
+      </div>
     </>
   );
 };
--- masida/src/components/Header/Header.tsx
+++ masida/src/components/Header/Header.tsx
@@ -1,7 +1,19 @@
-const Header: React.FC = () => {
+import style from "./Header.module.scss";
+import Image from "next/image";
+
+const Header = () => {
   return (
     <>
-      <h1>나는 헤더야 ㅋㅋ</h1>
+      <div className={style.main}>
+        <div className={style.main_left}>
+          <Image src="/logo.png" width={214} height={74} alt="logo" />
+        </div>
+        <div className={style.main_right}>
+          <span>칵테일 추천</span>
+          <span>주변 바 추천</span>
+          <span>종효님 환영합니다!</span>
+        </div>
+      </div>
     </>
   );
 };
