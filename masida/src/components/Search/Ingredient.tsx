2b65b82d72cc047e5d22cafdec9f68e307ac68b4
손종효 <jbson1998@naver.com>
2023-03-14 17:41:23 +0900

    feat: component modified Issue: S08P22B208-280

--- masida/src/components/Search/Ingredient.tsx
+++ masida/src/components/Search/Ingredient.tsx
@@ -1,16 +1,45 @@
 import { useState } from "react";
 import style from "./Ingredient.module.scss";
-import { searchedIngredient } from "../Search/Category";
+import { searchedIngredient, searchIngredient } from "../Search/Category";
 
 const Ingredient = (props: searchedIngredient) => {
-  // const [ingredient, isIngredient] = useState<string[]>([]);
   const { ingredient } = props;
+  let [inputvalue, setInputValue] = useState<string>("")
+  let [addedIngredient, setAddedIngredient] = useState<searchIngredient[]>([]);
+  let [listIngredient, setListIngredient] = useState<searchIngredient[]>([]);
 
-  //검색에서는 filter함수를 써야할 것같음. 값만 쏙 빼내오기 위해.
-  const searchIngredient = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const target = event.target as HTMLInputElement;
-    console.log(target.value); //값 찍힘.
+  //재료검색
+  const searchIngredient = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement;
+    setListIngredient(() => {
+      return ingredient.filter((ingre) => ingre.ingredient_name.includes(target.value)
+      );
+    })
+    //ingre.ingredient_name 이 target.value를 포함하고 있다면 그 요소들만 보여주기.
+    setInputValue(target.value);
   };
+
+  //재료추가
+  const addIngredient = (e : searchIngredient) => {
+    console.log(e.ingredient_add);
+    e.ingredient_add = true;
+    setAddedIngredient((prevInfo) => [...prevInfo, e]);
+  }
+
+  const removeIngredient = (e : searchIngredient) => {
+    console.log(e.ingredient_add);
+    e.ingredient_add = false;
+    console.log(addedIngredient);
+    
+    for(let i = 0; i < addedIngredient.length; i++) {
+      if(addedIngredient[i].ingredient_id === e.ingredient_id) {
+        addedIngredient.splice(i,1);
+        break;
+      }
+    }
+    setAddedIngredient(addedIngredient);
+  }
+
   return (
     <>
       <div className={style.ingredient}>
@@ -20,28 +49,29 @@ const Ingredient = (props: searchedIngredient) => {
             type="search"
             placeholder="Search"
             className={style.ingredient_form_input}
+            value={inputvalue}
             onChange={searchIngredient}
           />
         </div>
         <div className={style.ingredient_search_result_list}>
-          {ingredient.map((key) => (
+          {listIngredient.map((ingre) => (
             <>
-              {key.ingredient_add ? (
+              {ingre.ingredient_add ? (
                 <div></div>
               ) : (
-                <div className={style.ingredient_search_result_item}>
-                  {key.ingredient_name} +
+                <div className={style.ingredient_search_result_item} key={ingre.ingredient_id} onClick={() => addIngredient(ingre)}>
+                  {ingre.ingredient_name} +
                 </div>
               )}
             </>
           ))}
         </div>
         <div className={style.ingredient_search_added_list}>
-          {ingredient.map((key) => (
+          {addedIngredient.map((ingre) => (
             <>
-              {key.ingredient_add ? (
-                <div className={style.ingredient_search_added_item}>
-                  {key.ingredient_name} -
+              {ingre.ingredient_add ? (
+                <div className={style.ingredient_search_added_item} key={ingre.ingredient_id} onClick={() => removeIngredient(ingre)}>
+                  {ingre.ingredient_name} -
                 </div>
               ) : (
                 <div></div>
