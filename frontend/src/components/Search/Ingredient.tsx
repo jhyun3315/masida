a414ba5dcc00505e564ef20c130b4047996c6751
sonjonghyo <sjonghyo97@gmail.com>
2023-03-19 01:00:51 +0900

    fix: redux 오류 수정

--- frontend/src/components/Search/Ingredient.tsx
+++ frontend/src/components/Search/Ingredient.tsx
@@ -1,29 +1,47 @@
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { searchedIngredientType, searchIngredientType } from "../../type/ingredientTypes";
+import {
+  searchedIngredientType,
+  searchIngredientType,
+} from "../../type/ingredientTypes";
 import { RootState } from "../../../store/store";
 import { setSelectIngredient } from "../../../store/category/ingredientSlice";
 import style from "./Ingredient.module.scss";
 
 const Ingredient = (props: searchedIngredientType) => {
   const dispatch = useDispatch();
-  const selectIngredient = useSelector((state : RootState) => state.ingredientSelect.ingredient);
+  const selectIngredient = useSelector(
+    (state: RootState) => state.ingredientSelect.ingredient
+  );
   const { ingredient } = props;
   let [inputvalue, setInputValue] = useState<string>("");
-  let [addedIngredient, setAddedIngredient] = useState<searchIngredientType[]>([]);
-  let [listIngredient, setListIngredient] = useState<searchIngredientType[]>([]);
+  let [addedIngredient, setAddedIngredient] = useState<searchIngredientType[]>(
+    []
+  );
+  let [listIngredient, setListIngredient] = useState<searchIngredientType[]>(
+    []
+  );
 
   console.log(selectIngredient);
-  
+  let copy: searchIngredientType[] = Object.assign([], addedIngredient);
+  console.log(copy);
 
   useEffect(() => {
-    dispatch(setSelectIngredient(listIngredient));
-  },[listIngredient])
+    copy = Object.assign([], addedIngredient);
+    console.log(copy);
+  }, [addedIngredient]);
+
+  // 재료 추가, 삭제될때마다 redux에 재료 저장.(동작함)
+  useEffect(() => {
+    dispatch(setSelectIngredient(copy));
+  }, [addedIngredient]);
 
   //재료검색
   const searchIngredient = (e: React.ChangeEvent<HTMLInputElement>) => {
     const target = e.target as HTMLInputElement;
+
     setListIngredient(() => {
+      console.log(target.value);
       return ingredient.filter((ingre) =>
         ingre.ingredient_name.includes(target.value)
       );
@@ -36,22 +54,31 @@ const Ingredient = (props: searchedIngredientType) => {
     setInputValue(target.value);
   };
 
-  //재료추가(그냥 추가하면 e.ingredient_add를 변경하면 read_only라 변경이 불가능하다며 에러를 띄웁니다.)
+  //재료추가
   const addIngredient = (e: searchIngredientType) => {
-    console.log(e);
-    
     console.log(e.ingredient_add);
     e.ingredient_add = true;
     setAddedIngredient((prevInfo) => [...prevInfo, e]);
   };
 
+  //재료삭제(그냥 삭제하면 e.ingredient_add를 변경하면 read_only라 변경이 불가능하다며 에러를 띄웁니다.)
+  //Redux에 저장할 것이 useState의 변수라면 (dispatch에 useState변수로 선언한 변수가 들어가면) 해당 변수는 read-only입니다. 변경불가능.
+  //...인 스프레드 연산자는 얕은 복사입니다.
+  //깊은 복사 Object.assign 실패
   const removeIngredient = (e: searchIngredientType) => {
-    console.log(e.ingredient_add);
-    e.ingredient_add = false;
-    console.log(addedIngredient);
-    //id가 같은것은 빼주자.
+    // console.log(e.ingredient_add);
+    let findIndex = addedIngredient.findIndex(
+      (ingre) => ingre.ingredient_id === e.ingredient_id
+    );
+    let copyAddedIngredient: searchIngredientType[] = Object.assign(
+      [],
+      addedIngredient
+    );
+    //애초에 리덕스에 저장된것을 복사해와서 안되는 건가?..
+    copyAddedIngredient[findIndex].ingredient_add = false;
+    // e.ingredient_add = false;
     setAddedIngredient(
-      addedIngredient.filter((add) => add.ingredient_id !== e.ingredient_id)
+      copyAddedIngredient.filter((add) => add.ingredient_id !== e.ingredient_id)
     );
   };
 
--- frontend/store/category/ingredientSlice.ts
+++ frontend/store/category/ingredientSlice.ts
@@ -1,18 +1,31 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 import { PayloadAction } from "@reduxjs/toolkit";
-import { searchedIngredientType, searchIngredientType } from "../../src/type/ingredientTypes";
+import {
+  searchedIngredientType,
+  searchIngredientType,
+} from "../../src/type/ingredientTypes";
 
+//리덕스의 상태를 변경하기 위해선 원래 reducer안에사 action으로 변경해주어야함.
+//그냥 바로 접근하려 해서 에러를 띄운다고 현재 생각중.
+type stateType = {
+  add: searchedIngredientType;
+  remove: searchIngredientType;
+};
 
+//이부분이 잘못된 것 같음.
 const initialState: searchedIngredientType = {
   ingredient: [],
 };
 
 const counterSlice = createSlice({
-  name: 'ingredientSelect',
+  name: "ingredientSelect",
   initialState,
   reducers: {
-    setSelectIngredient : (state, action : PayloadAction<searchIngredientType[]>) => {
-      state.ingredient = action.payload ;
+    setSelectIngredient: (
+      state,
+      action: PayloadAction<searchIngredientType[]>
+    ) => {
+      state.ingredient = action.payload;
     },
   },
 });
