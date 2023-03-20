5c355bde41085dced82fb38a9c587a8f464ec3f6
juseong <kjschocolate@gmail.com>
2023-03-20 16:44:59 +0900

    쿠키에 refreshToken 저장

--- backend/src/main/java/com/ssafy/cocktail/backend/oauth/controller/OAuthController.java
+++ backend/src/main/java/com/ssafy/cocktail/backend/oauth/controller/OAuthController.java
@@ -1,9 +1,9 @@
 package com.ssafy.cocktail.backend.oauth.controller;
 
-//import com.ssafy.cocktail.backend.oauth.service.CustomOAuth2UserService;
 import com.ssafy.cocktail.backend.oauth.dto.UserInfo;
 import com.ssafy.cocktail.backend.oauth.dto.UserLoginRes;
 import com.ssafy.cocktail.backend.oauth.service.CustomOAuth2UserService;
+import com.ssafy.cocktail.backend.oauth.util.JwtTokenUtil;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,8 +28,10 @@ public class OAuthController {
         // 로그인 완료
         UserInfo userInfo = customOAuth2UserService.loginUser(code);
         Cookie cookie = new Cookie("refreshToken", userInfo.getRefreshToken());
-//        cookie.se
-//        cookie.setMaxAge();
+        cookie.setMaxAge(JwtTokenUtil.refreshExpirationTime);
+        cookie.setSecure(true); // 클라이언트가 HTTPS가 아닌 통신에서는 해당 쿠키를 전송하지 않도록 하는 설정
+        cookie.setHttpOnly(true); // 브라우저에서 쿠키에 접근할 수 없도록 하는 설정 (XSS 공격 방지)
+        cookie.setPath("/");
         return ResponseEntity.ok(UserLoginRes.of(200, "Sucess", userInfo.getAccessToken(), userInfo.getUserName()));
     }
 }
