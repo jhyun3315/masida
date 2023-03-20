5196b2edc433546864a2a8cfbaae8276894cf792
김지환 <tykimdream@naver.com>
2023-03-20 16:44:19 +0900

    feat : difficulty_img_url_converter added (pages-api-utility-difficulty_img_url_converter)

--- frontend/src/type/commentTypes.tsx
+++ frontend/src/type/commentTypes.tsx
@@ -1,10 +1,21 @@
+export type commentType = {
+  comment_id: number;
+  comment_content: string;
+  comment_rating: number;
+  comment_create_date: string;
+  comment_difficulty: string;
+  user_name: string;
+  user_profile: string;
+  writer_checker: boolean;
+};
+
 // 마이 페이지 댓글 정보
 export type mypageCommentType = {
-    cocktail_id: number;
-    cocktail_name_ko: string;
-    cocktail_img: string;
-    cocktail_difficulty_user: string;
-    comment_content: string;
-    comment_rating: number;
-    comment_date: String;
-  }
+  cocktail_id: number;
+  cocktail_name_ko: string;
+  cocktail_img: string;
+  cocktail_difficulty_user: string;
+  comment_content: string;
+  comment_rating: number;
+  comment_date: String;
+};
