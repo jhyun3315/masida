fbf0d9fa368238ff23ec36ab56bd9c3e8098c096
juseong <kjschocolate@gmail.com>
2023-03-21 09:18:05 +0900

    cocktail 엔티티 추가

---
+++ backend/src/main/java/com/ssafy/cocktail/backend/domain/entity/Cocktail.java
@@ -0,0 +1,35 @@
+package com.ssafy.cocktail.backend.domain.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access= AccessLevel.PUBLIC)
+public class Cocktail {
+    @Id
+    @GeneratedValue
+    @Column(name = "cocktail_id")
+    private Long id;
+
+    String cocktailNameKo;
+    String cocktailNameEn;
+    String cocktailImg;
+    String cocktailContent;
+    double cocktailRating;
+    String cocktailColor1;
+    String cocktailColor2;
+    String cocktailBase;
+    String cocktailGlass;
+    double cocktailDifficulty;
+    String cocktailDeleted;
+
+}
