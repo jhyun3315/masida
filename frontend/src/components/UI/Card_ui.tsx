c858be65a541d333147d33b0a74a9441121c8aa2
김영주 <eorhrtnv@naver.com>
2023-03-21 09:50:31 +0900

    Update MyPage

--- frontend/src/components/UI/Card_ui.tsx
+++ frontend/src/components/UI/Card_ui.tsx
@@ -1,5 +1,8 @@
 import style from "./Card_ui.module.scss";
-import { cocktailType,mypageCommentType } from "@/type/cocktailTypes";
+import { cocktailType, detail_props } from "@/type/cocktailTypes";
+import { difficulty_img_url_converter } from "@/pages/api/utility/difficulty_img_url_converter";
+import { mypageCommentType } from "@/type/commentTypes";
+
 import Link from "next/link";
 
 const Detail_recommend_card: React.FC<cocktailType> = (
@@ -38,21 +41,10 @@ const Detail_recommend_card: React.FC<cocktailType> = (
 };
 
 const Search_result_card: React.FC<cocktailType> = (cocktail: cocktailType) => {
-  let cocktail_difficulty_img_src = "";
-  switch (cocktail.cocktail_difficulty) {
-    case "상":
-      cocktail_difficulty_img_src = "/assets/icons/difficulty_HIGH.png";
-      break;
-    case "중":
-      cocktail_difficulty_img_src = "/assets/icons/difficulty_MID.png";
-      break;
-    case "하":
-      cocktail_difficulty_img_src = "/assets/icons/difficulty_LOW.png";
-      break;
-    default:
-      cocktail_difficulty_img_src = "/assets/icons/difficulty_LOW.png";
-      break;
-  }
+  let cocktail_difficulty_img_src = difficulty_img_url_converter(
+    cocktail.cocktail_difficulty
+  );
+
   return (
     <>
       <Link href={`/detail/${cocktail.cocktail_id}`}>
@@ -184,53 +176,98 @@ const My_like_card: React.FC<cocktailType> = (cocktail: cocktailType) => {
   );
 };
 
-const My_comment_card: React.FC<mypageCommentType> = (cocktail: mypageCommentType) => {
-  let cocktail_difficulty_img_src = "";
-  switch (cocktail.cocktail_difficulty_user) {
-     case "상":
-      cocktail_difficulty_img_src = "/assets/icons/difficulty_HIGH_MINI.png";
-      break;
-    case "중":
-      cocktail_difficulty_img_src = "/assets/icons/difficulty_MID_MINI.png";
-      break;
-    default:
-      cocktail_difficulty_img_src = "/assets/icons/difficulty_LOW_MINI.png";
-      break;
-  }
+const My_comment_card: React.FC<mypageCommentType> = (
+  cocktail: mypageCommentType
+) => {
   return (
     <>
-      <div className={ style.mypageComment}>
-        <div className={ style.mypageComment_img}>
+      <div>
+        <div>
           <Link href={`detail/${cocktail.cocktail_id}`}>
-            <img src={ cocktail.cocktail_img}></img>
+            <img src={cocktail.cocktail_img}></img>
           </Link>
         </div>
-        <div className={ style.mypageComment_title}>
-          <div className={style.mypageComment_title_date}>{ cocktail.comment_date}</div>
-          <div className={ style.mypageComment_title_name}>{cocktail.cocktail_name_ko}</div>
+        <div>
+          <div>{cocktail.comment_date}</div>
+          <div>{cocktail.cocktail_name_ko}</div>
         </div>
-        <div className={ style.mypageComment_rate}>
-          <img src="/assets/icons/ratingICON.png"/>
-          <span>{ cocktail.comment_rating}</span>
-          <img src={ cocktail_difficulty_img_src}/>
+        <div>
+          <img></img>
+          <span>{cocktail.cocktail_difficulty_user}</span>
+          <img></img>
         </div>
-        <div className={ style.mypageComment_content}>
-          { cocktail.comment_content}
-        </div>
+        <div>{cocktail.comment_content}</div>
       </div>
-     <hr/>
     </>
   );
 };
+const World_cup_league_card: React.FC<detail_props> = (
+  cocktail: detail_props
+) => {
+  // console.log(cocktail);
+  return (
+    <div className={style.flip}>
+      <div className={style.world_cup_card}>
+        <div className={style.world_cup_card_front}>
+          <div className={style.world_cup_top}>
+            <img
+              className={style.world_cup_cocktailImg}
+              src={cocktail.cocktail_img}
+              alt="사진이 없어요.../"
+            />
+          </div>
+          <div className={style.world_cup_bottom}>
+            <div className={style.world_cup_cocktail_name_ko}>
+              {cocktail.cocktail_name_ko}
+            </div>
+            <div className={style.world_cup_cocktail_name_en}>
+              {cocktail.cocktail_name_en}
+            </div>
+          </div>
+        </div>
 
+        <div className={style.world_cup_card_back}>
+          <div className={style.world_cup_title}>
+            <div className={style.world_cup_cocktail_name_ko}>
+              {cocktail.cocktail_name_ko}
+            </div>
+            <div className={style.world_cup_cocktail_name_en}>
+              {cocktail.cocktail_name_en}
+            </div>
+          </div>
+          <div className={style.ingredient_list}>
+            <img
+              className={style.ingredient_legendIMG}
+              src="/assets/icons/ingredient_legendIMG.png"
+              alt=""
+            />
+            {cocktail.recipe.map((key) => (
+              <div className={style.ingredient_list_element}>
+                {key.recipe_num}. {key.recipe_content}
+              </div>
+            ))}
+          </div>
+          <div className={style.recipe_content_textarea}>
+            {/* 소개 or 레시피 */}
+            <div className={style.recipe_content_title}>소개</div>
+            {/* {cocktail.recipe.map((key) => (
+              <div>{key.recipe_content}</div>
+            ))} */}
+            {cocktail.cocktail_content}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 // const AnalysisCardComponent: React.FC<AnalysisProps> = ({
 //   analysisCard,
 // }: AnalysisProps) => {
 //   return (
 //     <>
-//       <div className={style.analysisCard}>
-//         <div className={style.cocktailImg}></div>
-//         <div className={style.cocktailName}>{analysisCard.name}</div>
+//       <div className={styles.analysisCard}>
+//         <div className={styles.cocktailImg}></div>
+//         <div className={styles.cocktailName}>{analysisCard.name}</div>
 //       </div>
 //     </>
 //   );
@@ -241,5 +278,6 @@ export {
   Search_result_card,
   My_bookmark_card,
   My_like_card,
-  My_comment_card
+  My_comment_card,
+  World_cup_league_card,
 };
