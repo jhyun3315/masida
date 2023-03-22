58ff2ca7a834e231619437254bec64ac6d8f800c
juseong <kjschocolate@gmail.com>
2023-03-22 17:35:58 +0900

    해당 칵테일의 댓글들을 가져오는 JPA 추가

--- backend/src/main/java/com/ssafy/cocktail/backend/domain/repository/CommentRepository.java
+++ backend/src/main/java/com/ssafy/cocktail/backend/domain/repository/CommentRepository.java
@@ -1,7 +1,11 @@
 package com.ssafy.cocktail.backend.domain.repository;
 
+import com.ssafy.cocktail.backend.domain.entity.Cocktail;
 import com.ssafy.cocktail.backend.domain.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    public List<Comment> findAllByCocktail(Cocktail cocktail);
 }
