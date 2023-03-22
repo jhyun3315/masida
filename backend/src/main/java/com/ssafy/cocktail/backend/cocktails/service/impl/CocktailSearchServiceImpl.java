af1da7d6ab0f7f8a353486d61ce97240dedf348d
juseong <kjschocolate@gmail.com>
2023-03-22 10:57:56 +0900

    칵테일 검색 서비스 추가, 칵테일 재료 리스트 반환 추가

---
+++ backend/src/main/java/com/ssafy/cocktail/backend/cocktails/service/impl/CocktailSearchServiceImpl.java
@@ -0,0 +1,29 @@
+package com.ssafy.cocktail.backend.cocktails.service.impl;
+
+
+import com.ssafy.cocktail.backend.cocktails.dto.IngredientSearch;
+import com.ssafy.cocktail.backend.cocktails.service.CocktailSearchService;
+import com.ssafy.cocktail.backend.domain.entity.Ingredient;
+import com.ssafy.cocktail.backend.domain.repository.IngredientRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class CocktailSearchServiceImpl implements CocktailSearchService {
+
+    private IngredientRepository ingredientRepository;
+    @Override
+    public ArrayList<IngredientSearch> getIngredientSearchList() {
+        // 재료 검색에 필요한 모든 재료 리스트 반환
+        List<Ingredient> ingredients = ingredientRepository.findAll(); // 모든 재료 목록 가져오기
+        ArrayList<IngredientSearch> ingredientSearchList  = new ArrayList<>(); // 모든 재료 리스트
+        for (Ingredient ingredient: ingredients) { // 재료
+            ingredientSearchList.add(new IngredientSearch(ingredient.getId(), ingredient.getIngredientName())); // 재료 리스트에 추가
+        }
+        return ingredientSearchList;
+    }
+}
