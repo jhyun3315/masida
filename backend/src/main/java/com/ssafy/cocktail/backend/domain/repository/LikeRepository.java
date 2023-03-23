786e743431bdc8ce33725d0a525df599b93dc655
juseong <kjschocolate@gmail.com>
2023-03-23 09:49:41 +0900

    해당 칵테일의 유저 좋아요를 가져오는 JPA 추가

--- backend/src/main/java/com/ssafy/cocktail/backend/domain/repository/LikeRepository.java
+++ backend/src/main/java/com/ssafy/cocktail/backend/domain/repository/LikeRepository.java
@@ -2,10 +2,13 @@ package com.ssafy.cocktail.backend.domain.repository;
 
 import com.ssafy.cocktail.backend.domain.entity.Cocktail;
 import com.ssafy.cocktail.backend.domain.entity.Like;
+import com.ssafy.cocktail.backend.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.security.cert.CertPath;
 import java.util.List;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
     public List<Like> findAllByCocktail(Cocktail cocktail);
+    public Like findByUserAndCocktail(User user, Cocktail cocktail);
 }
