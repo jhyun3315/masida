6a989be96fd4f6a530165f78c3e9177c0a4e85dc
손종효 <jbson1998@naver.com>
2023-03-23 17:33:38 +0900

    feat : 메인 배너 수정 및 검색 기능 Issue : S08P22B208-441

--- frontend/src/components/Main/Main_banner.tsx
+++ frontend/src/components/Main/Main_banner.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import style from './Main_banner.module.scss';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -13,10 +14,23 @@ const Main_banner = () => {
     autoplay: true,
     autoplaySpeed: 3000
   }
+  const router = useRouter();
+  const goBegginer = () =>  {
+    router.push("/theme/begginer");
+  }
+
+  const goSpring = () => {
+    router.push("/theme/spring");
+  }
+
+  const goSummer = () => {
+    router.push("/theme/summer");
+  }
+
   return (
     <div className='carousel'>
         <Slider {...settings}>
-          <div className={style.mainBanner}>
+          <div className={style.mainBanner_black}>
             <Image src="/assets/image/mainbanner.png"
                   alt="image"
                   layout="fill"
@@ -32,9 +46,58 @@ const Main_banner = () => {
               <h3>당신의 취향을 그대로 담은 칵테일을 <br/> 집에서도 손쉽게</h3>
             </div>
           </div>
-          <div><h1>테마페이지 1</h1></div>
-          <div><h1>테마페이지 2</h1></div>
-          <div><h1>테마페이지 3</h1></div>
+          <div className={style.mainBanner_white}>
+            <Image src="/assets/image/banner_begginer.png"
+                  alt="image"
+                  layout="fill"
+                  objectFit="cover"
+                  objectPosition="center"
+                  onClick={goBegginer}
+                  />
+            <div className={style.mainHeader}>
+              <Link href="/cocktail-worldcup">칵테일 월드컵</Link>
+              <Link href="/search">칵테일 검색</Link>
+              <Link href="">로그인</Link>
+            </div>
+            <div className={style.mainTitle} onClick={goBegginer}>
+              <h1>입문자 이신가요?</h1>
+              <h3>당신의 취향을 그대로 담은 칵테일을 <br/> 집에서도 손쉽게</h3>
+            </div>
+          </div>
+          <div className={style.mainBanner_white}>
+            <Image src="/assets/image/banner_spring_1.png"
+                  alt="image"
+                  layout="fill"
+                  objectFit="cover"
+                  objectPosition="center"
+                  onClick={goSpring}/>
+            <div className={style.mainHeader}>
+              <Link href="/cocktail-worldcup">칵테일 월드컵</Link>
+              <Link href="/search">칵테일 검색</Link>
+              <Link href="">로그인</Link>
+            </div>
+            <div className={style.mainTitle} onClick={goSpring}>
+              <h1>상큼한 봄</h1>
+              <h3>봄에 맞는 칵테일 <br/> 궁금하지 않으신가요?</h3>
+            </div>
+          </div>
+          <div className={style.mainBanner_white}>
+            <Image src="/assets/image/banner_summer.png"
+                  alt="image"
+                  layout="fill"
+                  objectFit="cover"
+                  objectPosition="center" 
+                  onClick = {goSummer}/>
+            <div className={style.mainHeader}>
+              <Link href="/cocktail-worldcup">칵테일 월드컵</Link>
+              <Link href="/search">칵테일 검색</Link>
+              <Link href="">로그인</Link>
+            </div>
+            <div className={style.mainTitle} onClick = {goSummer}>
+              <h1>무더운 여름</h1>
+              <h3>시원하고 싶지 않으신가요?<br/>지금 바로 함께하시죠</h3>
+            </div>
+          </div>
         </Slider>
       </div>
   );
