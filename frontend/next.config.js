db80d19314746a9548e10e9796f99d0a4de4c3ef
김지환 <tykimdream@naver.com>
2023-03-24 14:54:53 +0900

    build : next.config.js images added

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images : {
+    loader: "akamai",
+    path: "/",
+  },
   api_domain: "https://j8b208.p.ssafy.io/",
 };
 
