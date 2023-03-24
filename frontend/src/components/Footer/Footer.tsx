f6eb83563c2f54f70d143ca15e26087065be81e4
손종효 <jbson1998@naver.com>
2023-03-24 15:11:15 +0900

    fix : 이미지 로더 추가

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
 import style from "./Footer.module.scss";
+import { imgLoader } from '../../utils/imgLoader';
+import { ImageLoaderProps } from 'next/image';
 
 const Footer = () => {
   return (
@@ -18,18 +20,22 @@ const Footer = () => {
         </div>
         <div className={style.main_right}>
           <Image src="/assets/image/footer_sns1.png"
+            loader={({ src, width, quality }: ImageLoaderProps) => imgLoader({ src, width, quality })}
             alt="image"
             width={32}
             height={32} />
           <Image src="/assets/image/footer_sns2.png"
+            loader={({ src, width, quality }: ImageLoaderProps) => imgLoader({ src, width, quality })}
             alt="image"
             width={32}
             height={32} />
           <Image src="/assets/image/footer_sns3.png"
+            loader={({ src, width, quality }: ImageLoaderProps) => imgLoader({ src, width, quality })}
             alt="image"
             width={32}
             height={32} />
           <Image src="/assets/image/footer_sns4.png"
+            loader={({ src, width, quality }: ImageLoaderProps) => imgLoader({ src, width, quality })}
             alt="image"
             width={32}
             height={32} />
---
+++ frontend/src/components/UI/CommentModal.tsx
@@ -0,0 +1,5 @@
+const CommentModal = () => {
+    
+}
+
+export default CommentModal;
