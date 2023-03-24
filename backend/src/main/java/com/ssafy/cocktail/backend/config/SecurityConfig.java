86d023b5c0565c82ea2d5e720ac5157bf772a021
juseong <kjschocolate@gmail.com>
2023-03-24 10:04:04 +0900

    SecurityConfig 수정

--- backend/src/main/java/com/ssafy/cocktail/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/ssafy/cocktail/backend/config/SecurityConfig.java
@@ -1,14 +1,10 @@
 package com.ssafy.cocktail.backend.config;
 
-import com.ssafy.cocktail.backend.oauth.service.CustomOAuth2UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 
 @Configuration
 @EnableWebSecurity
