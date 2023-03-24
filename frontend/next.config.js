4c3a48a7c14d4fb7827021505f43fa247e29ff62
김지환 <tykimdream@naver.com>
2023-03-24 14:34:31 +0900

    build : next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  api_domain: "https://j8b208.p.ssafy.io/",
 };
 
 module.exports = nextConfig;
@@ -8,15 +9,12 @@ module.exports = nextConfig;
 const withImages = require("next-images");
 module.exports = withImages();
 
-module.exports = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
-
 //페이지 로딩될때 오른쪽 하단에 뜨던 NextJS 삼각형 돌아가는거 끄기.
 module.exports = {
   devIndicators: {
     buildActivity: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
