38a02e2584e4d3f6160dfe6c89585ed0df05b93f
김지환 <tykimdream@naver.com>
2023-03-25 16:22:13 +0900

    build : cocktail info test

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -18,20 +18,20 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // exportPathMap: async function (
-  //   defaultPathMap,
-  //   { dev, dir, outDir, distDir, buildId }
-  // ) {
-  //   return {
-  //     "/": { page: "/" },
-  //     "/cocktail-worldcup": { page: "/worldcup" },
-  //     "/mypage": { page: "/mypage" },
-  //     "/search": { page: "/search" },
-  //     "/theme": { page: "/theme" },
-  //     "/user-analysis": { page: "/user-analysis" },
-  //     "/detail": { page: "/detail", query: { id: "cocktail-id" } },
-  //   };
-  // },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+      "/cocktail-worldcup": { page: "/worldcup" },
+      "/mypage": { page: "/mypage" },
+      "/search": { page: "/search" },
+      "/theme": { page: "/theme" },
+      "/user-analysis": { page: "/user-analysis" },
+      "/detail": { page: "/detail", query: { id: "cocktail-id" } },
+    };
+  },
   trailingSlash: true,
 };
 
