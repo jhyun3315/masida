41d3871f954d51959df8982cc1ab91afdab8e6e3
juseong <kjschocolate@gmail.com>
2023-03-25 21:53:46 +0900

    fix: 칵테일 랜덤으로 1개 가져오는 sql 문법 수정

--- backend/src/main/java/com/ssafy/cocktail/backend/domain/repository/CocktailRepository.java
+++ backend/src/main/java/com/ssafy/cocktail/backend/domain/repository/CocktailRepository.java
@@ -20,12 +20,12 @@ public interface CocktailRepository extends JpaRepository<Cocktail, Long> {
             "order by 'cnt' desc", nativeQuery = true)
     List<CocktailLikesInterface> findCocktailByLikes();
 
-    @Query(value = "select c.cocktail_id as cocktailId " +
-            ", c.cocktail_name_ko as cocktailNameKo " +
-            ", c.cocktail_name_en as cocktailNameEn " +
-            ", c.cocktail_img as cocktailImg " +
-            ", c.cocktail_rating as cocktailRating " +
-            " from cocktails" +
+    @Query(value = "select cocktail_id as cocktailId " +
+            ", cocktail_name_ko as cocktailNameKo " +
+            ", cocktail_name_en as cocktailNameEn " +
+            ", cocktail_img as cocktailImg " +
+            ", cocktail_rating as cocktailRating " +
+            "from cocktails " +
             "order by rand() limit 1", nativeQuery = true)
     CocktailLikesInterface getCocktailRandomOne();
 
