ae66019c9f218ba6067521d7c51e5aaa76dd393a
juseong <kjschocolate@gmail.com>
2023-03-25 21:09:33 +0900

    feat: 칵테일 좋아요 사위 10개 API 구현 완료

--- backend/src/main/java/com/ssafy/cocktail/backend/domain/repository/CocktailRepository.java
+++ backend/src/main/java/com/ssafy/cocktail/backend/domain/repository/CocktailRepository.java
@@ -1,17 +1,22 @@
 package com.ssafy.cocktail.backend.domain.repository;
 
+import com.ssafy.cocktail.backend.cocktails.dto.CocktailLikesInterface;
 import com.ssafy.cocktail.backend.domain.entity.Cocktail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import com.ssafy.cocktail.backend.cocktails.dto.CocktailSortedLikes;
 
 import java.util.List;
 
 public interface CocktailRepository extends JpaRepository<Cocktail, Long> {
-    @Query(value = "select c.cocktail_id, c.cocktail_name_ko, c.cocktail_name_en, c.cocktail_img, c.cocktail_rating, count(c.cocktail_id) as 'cnt' from cocktails c\n" +
-            "join likes l on c.cocktail_id = l.cocktail_id\n" +
-            "group by cocktail_id\n" +
+    @Query(value = "select c.cocktail_id as cocktailId " +
+            ", c.cocktail_name_ko as cocktailNameKo " +
+            ", c.cocktail_name_en as cocktailNameEn " +
+            ", c.cocktail_img as cocktailImg " +
+            ", c.cocktail_rating as cocktailRating " +
+            ", count(c.cocktail_id) as cnt " +
+            "from cocktails c " +
+            "join likes l on c.cocktail_id = l.cocktail_id " +
+            "group by cocktailId " +
             "order by 'cnt' desc", nativeQuery = true)
-    List<CocktailSortedLikes> findCocktailByLikes();
-//    List<CocktailSortedLikes> findCocktailByLikes();
+    List<CocktailLikesInterface> findCocktailByLikes();
 }
