4f75392a6d6e98dfe526490949612079c26f035a
ssjjhh <jbson1998@naver.com>
2023-03-27 00:10:35 +0900

    feat:댓글 모달

--- frontend/src/components/Modal/CommentModal.tsx
+++ frontend/src/components/Modal/CommentModal.tsx
@@ -1,9 +1,76 @@
 import { useState } from "react";
 import { Dispatch, SetStateAction } from "react";
 import Image from "next/image";
+import { commentType } from "../../type/commentTypes";
 import { imgLoader } from "../../utils/imgLoader";
 import { ImageLoaderProps } from "next/image";
 import style from "./CommentModal.module.scss";
+
+
+const comments: commentType[] = [
+    {
+      comment_id: 1,
+      comment_content:
+        "댓글이다 이건.",
+      comment_rating: 1,
+      comment_create_date: "2023-03-26",
+      comment_difficulty: "/assets/icons/difficulty_HIGH_MINI.png",
+      user_name: "내이름은 손종효 탐정이죠",
+      user_profile: "/assets/image/user_profile_img.png",
+      writer_checker: false,
+    },
+    {
+      comment_id: 2,
+      comment_content: "comment_content2",
+      comment_rating: 2,
+      comment_create_date: "comment_create_date2",
+      comment_difficulty: "/assets/icons/difficulty_LOW_MINI.png",
+      user_name: "user_name2",
+      user_profile: "/assets/image/user_profile_img.png",
+      writer_checker: true,
+    },
+    {
+      comment_id: 3,
+      comment_content: "comment_content3",
+      comment_rating: 3,
+      comment_create_date: "comment_create_date3",
+      comment_difficulty: "/assets/icons/difficulty_MID_MINI.png",
+      user_name: "user_name3",
+      user_profile: "/assets/image/user_profile_img.png",
+      writer_checker: false,
+    },
+    {
+      comment_id: 3,
+      comment_content: "comment_content3",
+      comment_rating: 3,
+      comment_create_date: "comment_create_date3",
+      comment_difficulty: "/assets/icons/difficulty_MID_MINI.png",
+      user_name: "user_name3",
+      user_profile: "/assets/image/user_profile_img.png",
+      writer_checker: true,
+    },
+    {
+      comment_id: 3,
+      comment_content: "comment_content3",
+      comment_rating: 3,
+      comment_create_date: "comment_create_date3",
+      comment_difficulty: "/assets/icons/difficulty_MID_MINI.png",
+      user_name: "user_name3",
+      user_profile: "/assets/image/user_profile_img.png",
+      writer_checker: false,
+    },
+    {
+      comment_id: 3,
+      comment_content: "comment_content3",
+      comment_rating: 3,
+      comment_create_date: "comment_create_date3",
+      comment_difficulty: "/assets/icons/difficulty_MID_MINI.png",
+      user_name: "user_name3",
+      user_profile: "/assets/image/user_profile_img.png",
+      writer_checker: true,
+    },
+  ];
+
 
 interface propsType {
   setVisible: Dispatch<SetStateAction<boolean>>;
@@ -56,8 +123,77 @@ const CommentModal: React.FunctionComponent<propsType> = ({
         onClick={toggleComment}
       />
       <div className={style.comment_list}>
-        여기에 이미지랑 이름 쓴 날짜 내용 난이도 별점 수정 삭제까지 다
-        넣어줘야해
+        {comments.map((key) => (
+          <div className={style.comment}>
+            <div className={style.comment_layout}>
+              <div className={style.comment_left}>
+                <div className={style.user_comment_profile}>
+                  <img
+                    className={style.user_profile_img}
+                    src={key.user_profile}
+                    alt=""
+                  />
+                </div>
+                <div className={style.user_comment_textarea}>
+                  <div className={style.user_comment_name_date}>
+                    <div className={style.user_comment_name}>
+                      {key.user_name}
+                    </div>
+                    <div className={style.user_comment_date}>
+                      {key.comment_create_date}
+                    </div>
+                  </div>
+                  <div className={style.user_comment_content}>
+                    {key.comment_content}
+                  </div>
+                </div>
+              </div>
+              <div className={style.comment_right}>
+                <div className={style.comment_difficulty_user_rating}>
+                  <div className={style.comment_difficulty}>
+                    <img
+                      className={style.comment_difficulty_img}
+                      src={key.comment_difficulty}
+                      alt=""
+                    />
+                  </div>
+                  <div className={style.comment_rating}>
+                    <div>
+                      <img
+                        className={style.comment_rating_img}
+                        src="/assets/icons/ratingICON.png"
+                        alt=""
+                      />
+                    </div>
+                    <div className={style.comment_rating_count}>
+                      {key.comment_rating}
+                    </div>
+                  </div>
+                </div>
+                {key.writer_checker &&
+                <div className={style.cocktail_comment_edit}>
+                  <div className={style.comment_modify}>
+                    <img
+                      className={style.comment_modify_img}
+                      src="/assets/icons/detail_cocktailcomment_modify.png"
+                      alt=""
+                    />
+                    수정
+                  </div>
+                  <div className={style.comment_modify}>
+                    <img
+                      className={style.comment_modify_img}
+                      src="/assets/icons/detail_cocktailcomment_delete.png"
+                      alt=""
+                    />
+                    삭제
+                  </div>
+                </div>
+                }
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
       <div className={style.comment_write}>
         <hr />
