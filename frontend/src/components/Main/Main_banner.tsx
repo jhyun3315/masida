d475a191940cca781abadaf31885d45f50953970
김영주 <eorhrtnv@naver.com>
2023-03-26 15:10:19 +0900

    sss

--- frontend/src/components/Main/Main_banner.tsx
+++ frontend/src/components/Main/Main_banner.tsx
@@ -10,12 +10,13 @@ import { RootState } from '../../../store/store';
 import { ImageLoaderProps } from 'next/image';
 import { imgLoader } from '../../utils/imgLoader';
 
-const Main_banner = () => {
+
+const Main_banner = (cookieString:string) => {
   const settings = {
     dots: true,
     infinite: true,
     speed: 100,
-    autoplay: true,
+    // autoplay: true,
     autoplaySpeed: 3000
   }
   const router = useRouter();
@@ -31,18 +32,14 @@ const Main_banner = () => {
     router.push("/theme/summer");
   }
 
-  
-const user = useSelector((state : RootState) => state.user);
-  console.log(user.accessToken);
-  
-
+  console.log(cookieString);
 
   return (
     <div className='carousel'>
         <Slider {...settings}>
           <div className={style.mainBanner_black}>
-          <Image
-            loader={({ src, width, quality }: ImageLoaderProps) => imgLoader({ src, width, quality })}
+            <Image
+                  loader={({ src, width, quality }: ImageLoaderProps) => imgLoader({ src, width, quality })}
                   src="/assets/image/mainbanner.png"
                   alt="image"
                   layout="fill"
@@ -59,25 +56,23 @@ const user = useSelector((state : RootState) => state.user);
             </div>
           </div>
           <div className={style.mainBanner_white}>
-          <Image
-            loader={({ src, width, quality }: ImageLoaderProps) => imgLoader({ src, width, quality })}
-            
-                  src="/assets/image/banner_begginer.png"
-            
-                  alt="image"
-                  layout="fill"
-                  objectFit="cover"
-                  objectPosition="center"
-                  onClick={goBegginer}
-                  />
+            <Image
+                loader={({ src, width, quality }: ImageLoaderProps) => imgLoader({ src, width, quality })}
+                src="/assets/image/banner_begginer.png"
+                alt="image"
+                layout="fill"
+                objectFit="cover"
+                objectPosition="center"
+                onClick={goBegginer}
+            />
             <div className={style.mainHeader}>
               <Link href="/cocktail-worldcup">칵테일 월드컵</Link>
               <Link href="/search">칵테일 검색</Link>
-              <Link href="">로그인</Link>
+              <Link href="/api/oauth/kakao/login">로그인</Link>
             </div>
-            <div className={style.mainTitle} onClick={goBegginer}>
-              <h1>입문자 이신가요?</h1>
-              <h3>당신의 취향을 그대로 담은 칵테일을 <br/> 집에서도 손쉽게</h3>
+            <div className={style.mainTitle2} onClick={goBegginer}>
+              <h1><strong>맛과 향, </strong>모두 즐기는 칵테일의 <br/>매력을 느껴보세요.</h1>
+              <h3>칵테일 입문자를 위한 다양한 칵테일을 추천해드립니다.</h3>
             </div>
           </div>
           <div className={style.mainBanner_white}>
@@ -94,9 +89,9 @@ const user = useSelector((state : RootState) => state.user);
               <Link href="/search">칵테일 검색</Link>
               <Link href="">로그인</Link>
             </div>
-            <div className={style.mainTitle} onClick={goSpring}>
-              <h1>상큼한 봄</h1>
-              <h3>봄에 맞는 칵테일 <br/> 궁금하지 않으신가요?</h3>
+            <div className={style.mainTitle3} onClick={goSpring}>
+              <h1>봄의 느낌을 담은 칵테일,<br/> 궁금하지 않으신가요?</h1>
+              <h3>봄에 어울리는 칵테일을 추천해드립니다.</h3>
             </div>
           </div>
           <div className={style.mainBanner_white}>
@@ -114,14 +109,20 @@ const user = useSelector((state : RootState) => state.user);
               <Link href="">로그인</Link>
             </div>
             <div className={style.mainTitle} onClick = {goSummer}>
-              <h1>무더운 여름</h1>
-              <h3>시원하고 싶지 않으신가요?<br/>지금 바로 함께하시죠</h3>
+              <h1>여름 햇살 아래 즐기는 칵테일이 궁금하신가요?</h1>
+              <h3>여름에 어울리는 다양한 칵테일을 추천해드립니다.</h3>
             </div>
           </div>
         </Slider>
       </div>
   );
- };
+};
+ 
+// Main_banner.getInitialProps= async () => { 
+//   const req = await fetch('/api/oauth/kakao/login');
+//   const json = await req.json();
+//   console.log(json);
+// }
 
 export default Main_banner;
 
