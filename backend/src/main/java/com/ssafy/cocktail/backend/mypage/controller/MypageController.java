00e31df8dac230d5ebbc1fcba1c7fb98ad614267
jis22u <jis22u@gmail.com>
2023-03-26 23:19:53 +0900

    feat: 좋아요, 북마크 controller 추가

--- backend/src/main/java/com/ssafy/cocktail/backend/mypage/controller/MypageController.java
+++ backend/src/main/java/com/ssafy/cocktail/backend/mypage/controller/MypageController.java
@@ -1,9 +1,14 @@
 package com.ssafy.cocktail.backend.mypage.controller;
 
+import com.ssafy.cocktail.backend.cocktails.dto.request.CocktailIDReq;
+import com.ssafy.cocktail.backend.domain.dto.BaseResponseBody;
+import com.ssafy.cocktail.backend.mypage.service.MypageBookmarkService;
+import com.ssafy.cocktail.backend.mypage.service.MypageCommentService;
+import com.ssafy.cocktail.backend.mypage.service.MypageLikeService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "mypage", description = "마이페이지 API")
 @RestController
@@ -11,10 +16,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/mypage")
 public class MypageController {
 
+	private final MypageLikeService mypageLikeService;
+	private final MypageBookmarkService mypageBookmarkService;
+	private final MypageCommentService mypageCommentService;
 
-//	@GetMapping
-//	public ResponseEntity<> getUserLikeBookmark() {
-//		return ResponseEntity.status(200).body());
-//	}
+	@GetMapping("/")
+	public ResponseEntity<?> getLikeBookmarkCnt(@RequestHeader("Authorization") String accessToken) {
+
+		return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
+	}
+
+	@GetMapping("/likes")
+	public ResponseEntity<?> getLikeCocktailList(@RequestHeader("Authorization") String accessToken) {
+
+		return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
+	}
+
+	@GetMapping("/bookmarks")
+	public ResponseEntity<?> getBookmarkCocktailList(@RequestHeader("Authorization") String accessToken) {
+
+		return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
+	}
+
 
 }
