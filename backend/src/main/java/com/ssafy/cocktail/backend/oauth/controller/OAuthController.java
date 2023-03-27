617f830d92eb921342703d959cea73db070a3b4c
juseong <kjschocolate@gmail.com>
2023-03-27 10:59:21 +0900

    fix: CORS 설정

--- backend/src/main/java/com/ssafy/cocktail/backend/oauth/controller/OAuthController.java
+++ backend/src/main/java/com/ssafy/cocktail/backend/oauth/controller/OAuthController.java
@@ -24,7 +24,7 @@ import java.io.IOException;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/oauth")
-@CrossOrigin(origins = "http://localhost:3000, https://j8b208.p.ssafy.io")
+@CrossOrigin(origins = {"http://localhost:3000", "https://j8b208.p.ssafy.io"})
 public class OAuthController {
     private final OAuthService oAuthService;
 
