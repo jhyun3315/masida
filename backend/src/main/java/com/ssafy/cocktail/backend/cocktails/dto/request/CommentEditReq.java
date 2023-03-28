cb2f32395cc974b6c299dda7f79cbedae9c3e926
juseong <kjschocolate@gmail.com>
2023-03-28 22:49:27 +0900

    feat: 댓글 수정 Request 추가

---
+++ backend/src/main/java/com/ssafy/cocktail/backend/cocktails/dto/request/CommentEditReq.java
@@ -0,0 +1,22 @@
+package com.ssafy.cocktail.backend.cocktails.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public class CommentEditReq {
+    @Schema(defaultValue = "댓글 id", example = "0")
+    @JsonProperty("comment_id")
+    private long commentId;
+
+    @Schema(defaultValue = "댓글 내용", example = "인생 칵테일")
+    @JsonProperty("comment_content")
+    private String commentContent;
+
+    @Schema(defaultValue = "칵테일 평점", example = "4.2")
+    @JsonProperty("comment_rating")
+    private Double commentRating;
+
+    @Schema(defaultValue = "칵테일 난이도", example = "중")
+    @JsonProperty("comment_difficulty")
+    private String commentDifficulty;
+}
