ac6dfefe2c184f64509343bb5e16908e6a62d673
juseong <kjschocolate@gmail.com>
2023-03-28 21:15:08 +0900

    fix: 댓글 등록 API 엔티티 데이터 타입에 맞게 수정

--- backend/src/main/java/com/ssafy/cocktail/backend/cocktails/service/impl/CommentServiceImpl.java
+++ backend/src/main/java/com/ssafy/cocktail/backend/cocktails/service/impl/CommentServiceImpl.java
@@ -26,31 +26,29 @@ public class CommentServiceImpl implements CommentService {
     public boolean registerComment(String cocktailId, CommentReq commentInfo, String accessToken) {
         User user = oAuthService.getUser(accessToken); // 사용자 가져오기
         Cocktail cocktail = cocktailRepository.findCocktailById(Long.valueOf(cocktailId)); // 칵테일 가져오기
-        Double curCocktailDiffculty =
+        double curCocktailDiffculty =
                 commentInfo.getCommentDifficulty().equals("하") ? 1.0 :
                 commentInfo.getCommentDifficulty().equals("중") ? 2.0 : 3.0;
-        Double curCocktailRating = commentInfo.getCommentRating();
+        double curCocktailRating = commentInfo.getCommentRating();
         commentRepository.save( // 댓글 저장
                 Comment.builder()
                         .commentContent(commentInfo.getCommentContent())
                         .commentRating(curCocktailRating)
                         .commentDifficulty(curCocktailDiffculty)
-                        .commentDeleted("N")
+                        .commentDeleted(false)
                         .commentCreatedDate(LocalDateTime.now())
                         .commentUpdateDate(LocalDateTime.now())
                         .user(user)
                         .cocktail(cocktail)
                         .build()
         );
-        List<Comment> commets = commentRepository.findAllByCocktailAndCommentDeleted(cocktail, "N"); // 칵테일 댓글 가져오기
+        List<Comment> commets = commentRepository.findAllByCocktailAndCommentDeleted(cocktail, false); // 칵테일 댓글 가져오기
         int commentSize = commets.size() - 1; // 칵테일 댓글 개수
-        Double prevCocktailDiffculty = cocktail.getCocktailDifficulty() == null
-                ? 2.0 : cocktail.getCocktailDifficulty(); // 저장된 칵테일 난이도 가져오기
-        Double newCocktailDiffculty = ((prevCocktailDiffculty * commentSize) + curCocktailDiffculty) / (commentSize + 1); // 새로운 난이도 계산
+        double prevCocktailDiffculty = cocktail.getCocktailDifficulty(); // 저장된 칵테일 난이도 가져오기
+        double newCocktailDiffculty = ((prevCocktailDiffculty * commentSize) + curCocktailDiffculty) / (commentSize + 1); // 새로운 난이도 계산
         cocktail.setCocktailDifficulty(newCocktailDiffculty); // 새로운 난이도 삽입
-        Double prevCocktailRating = cocktail.getCocktailRating() == null
-                ? 3.0 : cocktail.getCocktailRating(); // 저장된 칵테일 평점 가져오기
-        Double newCocktailRating = ((prevCocktailRating * commentSize) + curCocktailRating) / (commentSize + 1); // 새로운 평점 계산
+        double prevCocktailRating = cocktail.getCocktailRating(); // 저장된 칵테일 평점 가져오기
+        double newCocktailRating = ((prevCocktailRating * commentSize) + curCocktailRating) / (commentSize + 1); // 새로운 평점 계산
         cocktail.setCocktailRating(newCocktailRating); // 새로운 칵테일 평점 삽입
         cocktailRepository.save(cocktail); // 칵테일 난이도, 평점 업데이트
 
