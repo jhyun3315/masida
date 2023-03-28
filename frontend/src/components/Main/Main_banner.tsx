e844d5f5ba381f173458885be51b3e23414f0057
김영주 <eorhrtnv@naver.com>
2023-03-28 13:14:45 +0900

    s

--- frontend/src/components/Main/Main_banner.tsx
+++ frontend/src/components/Main/Main_banner.tsx
@@ -10,7 +10,9 @@ import 'slick-carousel/slick/slick-theme.css';
 import { ImageLoaderProps } from 'next/image';
 import { imgLoader } from '../../utils/imgLoader';
 import axios from 'axios';
-import { login, logout } from '../../../store/user/userSlice';
+import { login } from '../../../store/modules/user';
+import { logout } from '../../../store/user/userSlice';
+import { RootState } from '../../../store';
 
 
 const Main_banner = () => { 
@@ -34,10 +36,7 @@ const Main_banner = () => {
     router.push("/theme/summer");
   }
 
-  // const [tokenValue, setTokenValue] = useState<string>("");
-
   const accessToken = router.query.accessToken as string;
-  // setTokenValue(accessToken);
   
   const dispatch = useDispatch();
 
@@ -45,18 +44,21 @@ const Main_banner = () => {
     dispatch(login(accessToken));
   },);
 
+   const getAccessToken = useSelector(
+    (state: RootState) => state.counter.accessToken
+  );
+
   const onLogoutHandler = () => { 
     const logout: any = axios.get('/api/oauth/kakao/logout', {
       headers: {
         Authorization: accessToken,
       }
     }).then(() => dispatch(logout()));
-    // setTokenValue("");
     
     console.log(logout);
   }
 
-  console.log("앙 토큰 값 가져오기?");
+  console.log("앙 토큰 값 가져오기?",getAccessToken);
 
   return (
     <div className={ style.mainBanner}>
@@ -72,7 +74,7 @@ const Main_banner = () => {
             <div className={style.mainHeader}>
               <Link href="/cocktail-worldcup">칵테일 월드컵</Link>
               <Link href="/search">칵테일 검색</Link>
-              {accessToken ?(<button onClick={onLogoutHandler}>로그아웃</button>) :(<Link href="https://j8b208.p.ssafy.io/api/oauth/kakao/login">로그인</Link>)}
+              {getAccessToken ?(<button onClick={onLogoutHandler}>로그아웃</button>) :(<Link href="https://j8b208.p.ssafy.io/api/oauth/kakao/login">로그인</Link>)}
             </div>
             <div className={style.mainTitle}>
               <h1>MASIDA,</h1>
@@ -92,7 +94,7 @@ const Main_banner = () => {
             <div className={style.mainHeader}>
               <Link href="/cocktail-worldcup">칵테일 월드컵</Link>
               <Link href="/search">칵테일 검색</Link>
-              {accessToken ?(<button onClick={onLogoutHandler}>로그아웃</button>) :(<Link href="https://j8b208.p.ssafy.io/api/oauth/kakao/login">로그인</Link>)}
+              {getAccessToken ?(<button onClick={onLogoutHandler}>로그아웃</button>) :(<Link href="https://j8b208.p.ssafy.io/api/oauth/kakao/login">로그인</Link>)}
             </div>
             <div className={style.mainTitle2} onClick={goBegginer}>
               <h1><strong>맛과 향, </strong>모두 즐기는 칵테일의 <br/>매력을 느껴보세요.</h1>
@@ -111,7 +113,7 @@ const Main_banner = () => {
             <div className={style.mainHeader}>
               <Link href="/cocktail-worldcup">칵테일 월드컵</Link>
               <Link href="/search">칵테일 검색</Link>
-              {accessToken ?(<button onClick={onLogoutHandler}>로그아웃</button>) :(<Link href="https://j8b208.p.ssafy.io/api/oauth/kakao/login">로그인</Link>)}
+              {getAccessToken ?(<button onClick={onLogoutHandler}>로그아웃</button>) :(<Link href="https://j8b208.p.ssafy.io/api/oauth/kakao/login">로그인</Link>)}
             </div>
             <div className={style.mainTitle3} onClick={goSpring}>
               <h1>봄의 느낌을 담은 칵테일,<br/> 궁금하지 않으신가요?</h1>
@@ -130,7 +132,7 @@ const Main_banner = () => {
             <div className={style.mainHeader}>
               <Link href="/cocktail-worldcup">칵테일 월드컵</Link>
               <Link href="/search">칵테일 검색</Link>
-              {accessToken ?(<button onClick={onLogoutHandler}>로그아웃</button>) :(<Link href="https://j8b208.p.ssafy.io/api/oauth/kakao/login">로그인</Link>)}
+              {getAccessToken ?(<button onClick={onLogoutHandler}>로그아웃</button>) :(<Link href="https://j8b208.p.ssafy.io/api/oauth/kakao/login">로그인</Link>)}
             </div>
             <div className={style.mainTitle4} onClick = {goSummer}>
               <h1>여름 햇살 아래 즐기는 칵테일, 궁금하신가요?</h1>
