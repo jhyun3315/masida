1a60bbcc3fd3ebe911f4c077981ec7f196435158
juseong <kjschocolate@gmail.com>
2023-03-28 10:25:45 +0900

    feat: 댓글 엔티티 빌더 수정

--- backend/src/main/java/com/ssafy/cocktail/backend/cocktails/service/CommentService.java
+++ backend/src/main/java/com/ssafy/cocktail/backend/cocktails/service/CommentService.java
@@ -1,5 +1,7 @@
 package com.ssafy.cocktail.backend.cocktails.service;
 
+import com.ssafy.cocktail.backend.cocktails.dto.request.CommentReq;
+
 public interface CommentService {
-    public boolean registerComment();
+    public boolean registerComment(CommentReq req);
 }
--- backend/src/main/java/com/ssafy/cocktail/backend/cocktails/service/impl/CommentServiceImpl.java
+++ backend/src/main/java/com/ssafy/cocktail/backend/cocktails/service/impl/CommentServiceImpl.java
@@ -1,12 +1,15 @@
 package com.ssafy.cocktail.backend.cocktails.service.impl;
 
+import com.ssafy.cocktail.backend.cocktails.dto.request.CommentReq;
 import com.ssafy.cocktail.backend.cocktails.service.CommentService;
+import com.ssafy.cocktail.backend.domain.entity.Comment;
 import com.ssafy.cocktail.backend.domain.repository.CommentRepository;
 
 public class CommentServiceImpl implements CommentService {
     private CommentRepository commentRepository;
     @Override
-    public boolean registerComment() {
-        return false;
+    public boolean registerComment(CommentReq req) {
+        
+        return true;
     }
 }
