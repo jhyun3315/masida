b7b79f6ea115ddba4b1351c2783dd4f4bcad347b
juseong <kjschocolate@gmail.com>
2023-03-28 13:53:22 +0900

    feat: 댓글 등록 API 구현 완료

--- backend/src/main/java/com/ssafy/cocktail/backend/cocktails/service/impl/CommentServiceImpl.java
+++ backend/src/main/java/com/ssafy/cocktail/backend/cocktails/service/impl/CommentServiceImpl.java
@@ -29,10 +29,11 @@ public class CommentServiceImpl implements CommentService {
         Double curCocktailDiffculty =
                 commentInfo.getCommentDifficulty().equals("하") ? 1.0 :
                 commentInfo.getCommentDifficulty().equals("중") ? 2.0 : 3.0;
-        commentRepository.save(
+        Double curCocktailRating = commentInfo.getCommentRating();
+        commentRepository.save( // 댓글 저장
                 Comment.builder()
                         .commentContent(commentInfo.getCommentContent())
-                        .commentRating(commentInfo.getCommentRating())
+                        .commentRating(curCocktailRating)
                         .commentDifficulty(curCocktailDiffculty)
                         .commentDeleted("N")
                         .commentCreatedDate(LocalDateTime.now())
@@ -41,13 +42,17 @@ public class CommentServiceImpl implements CommentService {
                         .cocktail(cocktail)
                         .build()
         );
-        List<Comment> cocktails = commentRepository.findAllByCocktailAndCommentDeleted(cocktail, "N"); // 칵테일 댓글 가져오기
-        int commentSize = cocktails.size(); // 칵테일 댓글 개수
+        List<Comment> commets = commentRepository.findAllByCocktailAndCommentDeleted(cocktail, "N"); // 칵테일 댓글 가져오기
+        int commentSize = commets.size() - 1; // 칵테일 댓글 개수
         Double prevCocktailDiffculty = cocktail.getCocktailDifficulty() == null
                 ? 2.0 : cocktail.getCocktailDifficulty(); // 저장된 칵테일 난이도 가져오기
         Double newCocktailDiffculty = ((prevCocktailDiffculty * commentSize) + curCocktailDiffculty) / (commentSize + 1); // 새로운 난이도 계산
         cocktail.setCocktailDifficulty(newCocktailDiffculty); // 새로운 난이도 삽입
-        cocktailRepository.save(cocktail); // 칵테일 난이도 업데이트
+        Double prevCocktailRating = cocktail.getCocktailRating() == null
+                ? 3.0 : cocktail.getCocktailRating(); // 저장된 칵테일 평점 가져오기
+        Double newCocktailRating = ((prevCocktailRating * commentSize) + curCocktailRating) / (commentSize + 1); // 새로운 평점 계산
+        cocktail.setCocktailRating(newCocktailRating); // 새로운 칵테일 평점 삽입
+        cocktailRepository.save(cocktail); // 칵테일 난이도, 평점 업데이트
 
         return true;
     }
