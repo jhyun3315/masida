fecaad367a0d6f6dfac6107adade58e67d4f79ec
juseong <kjschocolate@gmail.com>
2023-03-28 13:30:32 +0900

    feat: 댓글 작성시 칵테일 난이도 업데이트 추가

--- backend/src/main/java/com/ssafy/cocktail/backend/cocktails/service/impl/CommentServiceImpl.java
+++ backend/src/main/java/com/ssafy/cocktail/backend/cocktails/service/impl/CommentServiceImpl.java
@@ -13,6 +13,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -25,14 +26,14 @@ public class CommentServiceImpl implements CommentService {
     public boolean registerComment(String cocktailId, CommentReq commentInfo, String accessToken) {
         User user = oAuthService.getUser(accessToken); // 사용자 가져오기
         Cocktail cocktail = cocktailRepository.findCocktailById(Long.valueOf(cocktailId)); // 칵테일 가져오기
-        Double cocktailDiffculty =
+        Double curCocktailDiffculty =
                 commentInfo.getCommentDifficulty().equals("하") ? 1.0 :
                 commentInfo.getCommentDifficulty().equals("중") ? 2.0 : 3.0;
         commentRepository.save(
                 Comment.builder()
                         .commentContent(commentInfo.getCommentContent())
                         .commentRating(commentInfo.getCommentRating())
-                        .commentDifficulty(cocktailDiffculty)
+                        .commentDifficulty(curCocktailDiffculty)
                         .commentDeleted("N")
                         .commentCreatedDate(LocalDateTime.now())
                         .commentUpdateDate(LocalDateTime.now())
@@ -40,6 +41,14 @@ public class CommentServiceImpl implements CommentService {
                         .cocktail(cocktail)
                         .build()
         );
+        List<Comment> cocktails = commentRepository.findAllByCocktailAndCommentDeleted(cocktail, "N"); // 칵테일 댓글 가져오기
+        int commentSize = cocktails.size(); // 칵테일 댓글 개수
+        Double prevCocktailDiffculty = cocktail.getCocktailDifficulty() == null
+                ? 2.0 : cocktail.getCocktailDifficulty(); // 저장된 칵테일 난이도 가져오기
+        Double newCocktailDiffculty = ((prevCocktailDiffculty * commentSize) + curCocktailDiffculty) / (commentSize + 1); // 새로운 난이도 계산
+        cocktail.setCocktailDifficulty(newCocktailDiffculty); // 새로운 난이도 삽입
+        cocktailRepository.save(cocktail); // 칵테일 난이도 업데이트
+
         return true;
     }
 }
