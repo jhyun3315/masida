900cd2cf655dd1a1015018836fdcb8f1849e2c36
juseong <kjschocolate@gmail.com>
2023-03-29 15:55:11 +0900

    feat: 월드컵 서비스 추가

---
+++ backend/src/main/java/com/ssafy/cocktail/backend/worldcups/service/WorldCupService.java
@@ -0,0 +1,4 @@
+package com.ssafy.cocktail.backend.worldcups.service;
+
+public interface WorldCupService {
+}
---
+++ backend/src/main/java/com/ssafy/cocktail/backend/worldcups/service/impl/WorldCupImpl.java
@@ -0,0 +1,6 @@
+package com.ssafy.cocktail.backend.worldcups.service.impl;
+
+import com.ssafy.cocktail.backend.worldcups.service.WorldCupService;
+
+public class WorldCupImpl implements WorldCupService {
+}
