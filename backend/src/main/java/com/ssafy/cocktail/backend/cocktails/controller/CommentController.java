46c82ac25c0394b877529f9cdafa2ab46a2386b4
juseong <kjschocolate@gmail.com>
2023-03-29 12:32:05 +0900

    fix: 댓글 예외 처리 수정

--- backend/src/main/java/com/ssafy/cocktail/backend/cocktails/controller/CommentController.java
+++ backend/src/main/java/com/ssafy/cocktail/backend/cocktails/controller/CommentController.java
@@ -5,14 +5,11 @@ import com.ssafy.cocktail.backend.cocktails.dto.request.CommentReq;
 import com.ssafy.cocktail.backend.cocktails.dto.response.CommentRes;
 import com.ssafy.cocktail.backend.cocktails.service.CommentService;
 import com.ssafy.cocktail.backend.domain.dto.BaseResponseBody;
-import com.ssafy.cocktail.backend.oauth.service.OAuthService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.ArrayList;
 
 @Tag(name = "comment", description = "댓글 API")
@@ -22,46 +19,37 @@ import java.util.ArrayList;
 @RequestMapping("/api/comments")
 public class CommentController {
     private CommentService commentService;
-    private OAuthService oAuthService;
 
     @GetMapping("/{cocktail_id}")
-    public ResponseEntity<CommentRes> getComments(@RequestHeader("Authorization") String accessToken, @PathVariable("cocktail_id") String cocktailId, HttpServletResponse response) throws IOException {
+    public ResponseEntity<CommentRes> getComments(@RequestHeader("Authorization") String accessToken, @PathVariable("cocktail_id") String cocktailId) {
         ArrayList<CommentDetail> commentDetails = commentService.getComments(cocktailId, accessToken);
         if (commentDetails != null) {
             return ResponseEntity.status(200).body(CommentRes.of(200, "Success", commentDetails));
         }
-        String loginPageUri = oAuthService.loginPage(); // 로그인 페이지 가져오기
-        response.sendRedirect(loginPageUri); // 로그인 페이지로 이동
-        return ResponseEntity.status(404).body(CommentRes.of(404, "user not found", commentDetails));
+        return ResponseEntity.status(404).body(CommentRes.of(404, "please reLogin", commentDetails));
     }
 
     @PostMapping("/{cocktail_id}")
-    public ResponseEntity<?> saveComment(@RequestHeader("Authorization") String accessToken, @PathVariable("cocktail_id") String cocktailId, @RequestBody CommentReq req, HttpServletResponse response) throws IOException {
+    public ResponseEntity<?> saveComment(@RequestHeader("Authorization") String accessToken, @PathVariable("cocktail_id") String cocktailId, @RequestBody CommentReq req) {
         if (commentService.saveOrUpdateComment(cocktailId, null, req, accessToken)) {
             return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
         }
-        String loginPageUri = oAuthService.loginPage(); // 로그인 페이지 가져오기
-        response.sendRedirect(loginPageUri); // 로그인 페이지로 이동
-        return ResponseEntity.status(404).body(BaseResponseBody.of(404, "user not found"));
+        return ResponseEntity.status(404).body(BaseResponseBody.of(404, "please reLogin"));
     }
 
     @PutMapping("/{cocktail_id}/{comment_id}")
-    public ResponseEntity<?> updateComment(@RequestHeader("Authorization") String accessToken, @PathVariable("cocktail_id") String cocktailId, @PathVariable("comment_id") String commentId, @RequestBody CommentReq req, HttpServletResponse response) throws IOException {
+    public ResponseEntity<?> updateComment(@RequestHeader("Authorization") String accessToken, @PathVariable("cocktail_id") String cocktailId, @PathVariable("comment_id") String commentId, @RequestBody CommentReq req) {
         if (commentService.saveOrUpdateComment(cocktailId, commentId, req, accessToken)) {
             return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
         }
-        String loginPageUri = oAuthService.loginPage(); // 로그인 페이지 가져오기
-        response.sendRedirect(loginPageUri); // 로그인 페이지로 이동
-        return ResponseEntity.status(404).body(BaseResponseBody.of(404, "user not found"));
+        return ResponseEntity.status(404).body(BaseResponseBody.of(404, "please reLogin"));
     }
 
     @DeleteMapping("/{cocktail_id}/{comment_id}")
-    public ResponseEntity<?> removeComment(@RequestHeader("Authorization") String accessToken, @PathVariable("cocktail_id") String cocktailId, @PathVariable("comment_id") String commentId, HttpServletResponse response) throws IOException {
+    public ResponseEntity<?> removeComment(@RequestHeader("Authorization") String accessToken, @PathVariable("cocktail_id") String cocktailId, @PathVariable("comment_id") String commentId) {
         if (commentService.removeComment(cocktailId, commentId, accessToken)) {
             return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
         }
-        String loginPageUri = oAuthService.loginPage(); // 로그인 페이지 가져오기
-        response.sendRedirect(loginPageUri); // 로그인 페이지로 이동
-        return ResponseEntity.status(404).body(BaseResponseBody.of(404, "Fail"));
+        return ResponseEntity.status(404).body(BaseResponseBody.of(404, "please reLogin"));
     }
 }
