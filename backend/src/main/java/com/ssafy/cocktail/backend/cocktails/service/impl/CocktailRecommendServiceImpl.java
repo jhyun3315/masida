e0e2a3f02d6db93ec75db25d066c04dff5883140
juseong <kjschocolate@gmail.com>
2023-03-30 10:46:29 +0900

    feat: 색상 추천 레파지토리 추가

--- backend/src/main/java/com/ssafy/cocktail/backend/cocktails/service/impl/CocktailRecommendServiceImpl.java
+++ backend/src/main/java/com/ssafy/cocktail/backend/cocktails/service/impl/CocktailRecommendServiceImpl.java
@@ -3,6 +3,8 @@ package com.ssafy.cocktail.backend.cocktails.service.impl;
 import com.ssafy.cocktail.backend.cocktails.dto.CocktailRecommendDetail;
 import com.ssafy.cocktail.backend.cocktails.service.CocktailRecommendService;
 import com.ssafy.cocktail.backend.domain.entity.RecommendIngredient;
+import com.ssafy.cocktail.backend.domain.repository.RecommendColorRepository;
+import com.ssafy.cocktail.backend.domain.repository.RecommendIngredientRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +14,15 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class CocktailRecommendServiceImpl implements CocktailRecommendService {
+    private RecommendIngredientRepository recommendIngredientRepository;
+    private RecommendColorRepository recommendColorRepository;
+
     @Override
     public ArrayList<CocktailRecommendDetail> getRecommendCocktails(int op, String accessToken) {
         ArrayList<CocktailRecommendDetail> results = new ArrayList<>();
+        List<?> recommends = new ArrayList<>();
         if (op == 0) {
-//            List<RecommendIngredient> recommendIngredients =
+//            recommends =
         } else {
 
         }
---
+++ backend/src/main/java/com/ssafy/cocktail/backend/domain/repository/RecommendColorRepository.java
@@ -0,0 +1,7 @@
+package com.ssafy.cocktail.backend.domain.repository;
+
+import com.ssafy.cocktail.backend.domain.entity.RecommendColor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecommendColorRepository extends JpaRepository<RecommendColor, Long> {
+}
