1e04ad476a0fafc4bced5300bbbdc808bc064277
jis22u <jis22u@gmail.com>
2023-03-30 16:42:24 +0900

    feat: 댓글칵테일 dto 생성

--- backend/src/main/java/com/ssafy/cocktail/backend/myPage/controller/MypageController.java
+++ backend/src/main/java/com/ssafy/cocktail/backend/myPage/controller/MypageController.java
@@ -1,12 +1,12 @@
 package com.ssafy.cocktail.backend.myPage.controller;
 
-import com.ssafy.cocktail.backend.domain.dto.BaseResponseBody;
 import com.ssafy.cocktail.backend.domain.entity.User;
 import com.ssafy.cocktail.backend.myPage.dto.LikeBookmarkCnt;
 import com.ssafy.cocktail.backend.myPage.dto.LikeBookmarkCocktail;
 import com.ssafy.cocktail.backend.myPage.dto.response.LikeBookmarkCntRes;
 import com.ssafy.cocktail.backend.myPage.dto.response.LikeBookmarkCocktailsRes;
 import com.ssafy.cocktail.backend.myPage.service.MypageBookmarkService;
+import com.ssafy.cocktail.backend.myPage.service.MypageCommentService;
 import com.ssafy.cocktail.backend.myPage.service.MypageLikeService;
 import com.ssafy.cocktail.backend.oauth.service.OAuthService;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -28,7 +28,7 @@ public class MypageController {
 	private final OAuthService oAuthService;
 	private final MypageLikeService mypageLikeService;
 	private final MypageBookmarkService mypageBookmarkService;
-//	private final MypageCommentService mypageCommentService;
+	private final MypageCommentService mypageCommentService;
 
 	@GetMapping("/cnt")
 	public ResponseEntity<?> getLikeBookmarkCnt(@RequestHeader Map<String, String> data) {
@@ -108,4 +108,29 @@ public class MypageController {
 			return ResponseEntity.status(400).body(LikeBookmarkCocktailsRes.of(400, "존재하지 않는 사용자입니다.", new ArrayList<>()));
 		}
 	}
+
+	@GetMapping("/comment")
+	public ResponseEntity<?> getCommentCocktailList(@RequestHeader Map<String, String> data) {
+		String accessToken = data.get("authorization");
+
+		// 토큰이 없는 경우,
+		if (accessToken == null) {
+			return ResponseEntity.status(400).body(LikeBookmarkCocktailsRes.of(400, "토큰이 비어있습니다", new ArrayList<>()));
+		}
+
+		// 토큰이 유효한 경우,
+		try {
+			// 해당 사용자 가져오기
+			User user = oAuthService.getUser(accessToken);
+
+			// 해당 유저가 북마크한 칵테일 리스트
+			List<LikeBookmarkCocktail> bookmarkCocktailList = mypageBookmarkService.getBookmarkCocktailList(user.getId());
+			return ResponseEntity.status(200).body(LikeBookmarkCocktailsRes.of(200, "Success", bookmarkCocktailList));
+		}
+		// 토큰이 유효하지 않은 경우
+		catch (Exception e) {
+			return ResponseEntity.status(400).body(LikeBookmarkCocktailsRes.of(400, "존재하지 않는 사용자입니다.", new ArrayList<>()));
+		}
+	}
+
 }
