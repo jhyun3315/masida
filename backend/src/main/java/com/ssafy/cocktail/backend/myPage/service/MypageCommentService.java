4da947ba1b93b1420e5811c8e42418a51def98b0
jis22u <jis22u@gmail.com>
2023-03-31 16:25:40 +0900

    feat: 댓글 및 평점 칵테일 페이지네이션 적용

--- backend/src/main/java/com/ssafy/cocktail/backend/myPage/service/MypageCommentService.java
+++ backend/src/main/java/com/ssafy/cocktail/backend/myPage/service/MypageCommentService.java
@@ -1,9 +1,12 @@
 package com.ssafy.cocktail.backend.myPage.service;
 
 import com.ssafy.cocktail.backend.myPage.dto.CommentCocktail;
+import com.ssafy.cocktail.backend.myPage.dto.PaginationDataSet;
+import com.ssafy.cocktail.backend.myPage.dto.response.PageableRes;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface MypageCommentService {
-	public List<CommentCocktail> getCommentCocktailList(Long userId);
+	public PaginationDataSet getCommentCocktailList(Long userId, Pageable pageable);
 }
