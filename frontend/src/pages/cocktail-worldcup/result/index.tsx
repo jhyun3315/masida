093aa35a384671f78de5ebc516a23ae3b3b65ea7
김영주 <eorhrtnv@naver.com>
2023-04-02 22:07:13 +0900

    fix UserAnalisys

--- frontend/src/pages/cocktail-worldcup/result/index.tsx
+++ frontend/src/pages/cocktail-worldcup/result/index.tsx
@@ -1,16 +1,5 @@
 import World_cup_league_result from "../../../components/Worldcup/World_cup_league_result";
 import Header from "../../../components/Header/Header";
-<<<<<<< HEAD
-import { cocktail_worldcup_data, detail_props } from "../../../type/cocktailTypes";
-
-const cocktail_worldcup_result = (props: cocktail_worldcup_data) => {
-    return (
-      <>
-        <Header />
-        <World_cup_league_result {...props} />
-      </>
-    );
-=======
 import { detail_props } from "../../../type/cocktailTypes";
 import { cocktail_worldcup_data } from "../../../type/cocktailTypes";
 import ResetCategory from "../../../components/UI/ResetCategory";
@@ -23,6 +12,5 @@ const cocktail_worldcup_result = (props: cocktail_worldcup_data) => {
       <World_cup_league_result {...props} />
     </>
   );
->>>>>>> 3821500253f7be0eb91aa972b0e7ce033ab83a84
 };
 export default cocktail_worldcup_result;
