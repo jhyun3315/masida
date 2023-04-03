a27c5fbfdd7675a6a3bdadcb0bb8305d49805c35
이지현 <jiannalee3@gmail.com>
2023-04-03 12:43:24 +0900

    색상 RBG값 변경

--- backend/src/main/java/com/ssafy/cocktail/backend/myAnalysis/dto/ColorRGB.java
+++ backend/src/main/java/com/ssafy/cocktail/backend/myAnalysis/dto/ColorRGB.java
@@ -1,16 +1,16 @@
 package com.ssafy.cocktail.backend.myAnalysis.dto;
 
 public enum ColorRGB {
-    빨간색("#F00A0A"),
-    주황색("#FF9600"),
+    빨간색("#E02A1C"),
+    주황색("#FFAD03"),
     분홍색("#FF99CC"),
-    노란색("#FFFF00"),
-    초록색("#0A820A"),
-    파란색("#0000FF"),
-    남색("#000080"),
-    보라색("#B400BE"),
-    갈색("#964B00"),
-    흰색("#ffffff");
+    노란색("#D0B83A"),
+    초록색("#235C18"),
+    파란색("#0000E1"),
+    남색("#2E354F"),
+    보라색("#C800FF"),
+    갈색("#B26132"),
+    흰색("#CCC8C8");
 
     private final String rgb;
     ColorRGB(String rgb){
