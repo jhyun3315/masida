585f60e445963d9a0d864eb9d6f7f9be001b3ef8
이지현 <jiannalee3@gmail.com>
2023-04-03 10:10:17 +0900

    마이페이지 상세 - 비슷한 사용자 선호 칵테일 API 수정

--- backend/src/main/java/com/ssafy/cocktail/backend/myAnalysis/dto/MyAnalysisOthersInterface.java
+++ backend/src/main/java/com/ssafy/cocktail/backend/myAnalysis/dto/MyAnalysisOthersInterface.java
@@ -2,6 +2,7 @@ package com.ssafy.cocktail.backend.myAnalysis.dto;
 
 public interface MyAnalysisOthersInterface {
     String getCocktailNameKo();
+    String getCocktailNameEn();
     int getCocktailCount();
     int getCocktailRatio();
 }
