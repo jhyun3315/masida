4b2344c7a2dbf1e7da7dd51ccbf3b9ebbc6a7b80
이지현 <jiannalee3@gmail.com>
2023-04-03 20:42:40 +0900

    마이페이지 상세 - 재료 별점분포 API 완료

--- backend/src/main/java/com/ssafy/cocktail/backend/myAnalysis/service/impl/MyAnalysisUserServiceImpl.java
+++ backend/src/main/java/com/ssafy/cocktail/backend/myAnalysis/service/impl/MyAnalysisUserServiceImpl.java
@@ -28,66 +28,66 @@ public class MyAnalysisUserServiceImpl implements MyAnalysisUserService {
 
     @Override
     public ArrayList<MyAnalysisBase> getAnalysisByBase(String accessToken) {
-        try{
+        try {
             User user = oAuthService.getUser(accessToken);
             ArrayList<MyAnalysisBaseInterface> interfaceArrayList = myAnalysisRepository.getMyAnalysisBaseList(user.getId());
             ArrayList<MyAnalysisBase> myAnalysisBaseArrayList = new ArrayList<>();
 
-            for(MyAnalysisBaseInterface ele : interfaceArrayList){
+            for (MyAnalysisBaseInterface ele : interfaceArrayList) {
                 myAnalysisBaseArrayList.add(new MyAnalysisBase(ele.getBaseName(), ele.getBaseCount(), ele.getBaseRatio()));
             }
             return myAnalysisBaseArrayList;
-        }catch (Exception e){
+        } catch (Exception e) {
             return new ArrayList<>();
         }
     }
 
     @Override
     public ArrayList<MyAnalysisColor> getAnalysisByColor(String accessToken) {
-        try{
-            User user =  oAuthService.getUser(accessToken);
+        try {
+            User user = oAuthService.getUser(accessToken);
             ArrayList<MyAnalysisColorInterface> interfaceArrayList = myAnalysisRepository.getMyAnalysisColorList(user.getId());
             ArrayList<MyAnalysisColor> myAnalysisColorArrayList = new ArrayList<>();
 
-            for(MyAnalysisColorInterface ele : interfaceArrayList){
+            for (MyAnalysisColorInterface ele : interfaceArrayList) {
                 ColorRGB rgb = ColorRGB.valueOf(ele.getColorName());
-                myAnalysisColorArrayList.add(new MyAnalysisColor(ele.getColorName(),rgb.getValue(), ele.getColorCount(), ele.getColorRatio()));
+                myAnalysisColorArrayList.add(new MyAnalysisColor(ele.getColorName(), rgb.getValue(), ele.getColorCount(), ele.getColorRatio()));
             }
             return myAnalysisColorArrayList;
-        }catch (Exception e){
+        } catch (Exception e) {
             return new ArrayList<>();
         }
     }
 
     @Override
     public ArrayList<MyAnalysisIngredient> getAnalysisByIngredient(String accessToken) {
-        try{
-            User user =  oAuthService.getUser(accessToken);
+        try {
+            User user = oAuthService.getUser(accessToken);
             ArrayList<MyAnalysisIngredientInterface> interfaceArrayList = myAnalysisRepository.getMyAnalysisIngredientList(user.getId());
             ArrayList<MyAnalysisIngredient> myAnalysisIngredientArrayList = new ArrayList<>();
 
-            for(MyAnalysisIngredientInterface ele : interfaceArrayList){
+            for (MyAnalysisIngredientInterface ele : interfaceArrayList) {
                 myAnalysisIngredientArrayList.add(new MyAnalysisIngredient(ele.getIngredientName(), ele.getIngredientCount(), ele.getIngredientRatio()));
             }
             return myAnalysisIngredientArrayList;
-        }catch (Exception e){
+        } catch (Exception e) {
             return new ArrayList<>();
         }
     }
 
     @Override
     public ArrayList<MyAnalysisOthers> getAnalysisByOthers(String accessToken) {
-        try{
-            User user =  oAuthService.getUser(accessToken);
+        try {
+            User user = oAuthService.getUser(accessToken);
 
-            ArrayList<MyAnalysisOthersInterface> interfaceArrayList = myAnalysisRepository.getMyAnalysisOthersList(user.getId(), user.getUserGender(),user.getUserAgeRange());
+            ArrayList<MyAnalysisOthersInterface> interfaceArrayList = myAnalysisRepository.getMyAnalysisOthersList(user.getId(), user.getUserGender(), user.getUserAgeRange());
             ArrayList<MyAnalysisOthers> myAnalysisOthersArrayList = new ArrayList<>();
 
-            for(MyAnalysisOthersInterface ele : interfaceArrayList){
+            for (MyAnalysisOthersInterface ele : interfaceArrayList) {
                 myAnalysisOthersArrayList.add(new MyAnalysisOthers(ele.getCocktailNameKo(), ele.getCocktailNameEn(), ele.getCocktailCount(), ele.getCocktailRatio()));
             }
             return myAnalysisOthersArrayList;
-        }catch (Exception e){
+        } catch (Exception e) {
             return new ArrayList<>();
         }
     }
@@ -100,53 +100,52 @@ public class MyAnalysisUserServiceImpl implements MyAnalysisUserService {
         // 전체 별점 점수
         int total_rating = 0;
         // 별점 전체 개수
-        int rating_count =0;
+        int rating_count = 0;
         // 최대 별점
         int rating_max = Integer.MIN_VALUE;
         // 최대 별점 베이스
         String rating_max_base = "";
 
-        try{
-            User user =  oAuthService.getUser(accessToken);
+        try {
+            User user = oAuthService.getUser(accessToken);
             ArrayList<MyAnalysisRatingBaseInterface> interfaceArrayList = myAnalysisRepository.getMyAnalysisRatingBaseList(user.getId());
             ArrayList<HashMap<String, Integer>> myAnalysisRatingBaseArrayList = new ArrayList<>();
-            HashMap<Integer, HashMap<String,Integer>> map = new HashMap<>();
+            HashMap<Integer, HashMap<String, Integer>> map = new HashMap<>();
 
             for (int i = 1; i <= 5; i++) map.put(i, new RatingBaseObj(i).getBMap());
 
             //데이터 정제 필요 ***
-            for(MyAnalysisRatingBaseInterface ele : interfaceArrayList){
+            for (MyAnalysisRatingBaseInterface ele : interfaceArrayList) {
                 int rating = ele.getRatingScore();
                 int base_count = ele.getBaseCount();
                 BaseEnum base = BaseEnum.valueOf(ele.getBaseName());
                 String base_name = base.getValue();
 
                 rating_count += base_count;
-                total_rating +=rating;
+                total_rating += rating;
 
                 // 최대 별점이라면, 최대 별점 베이스 & 최대 별점 갱신
-                if(rating > rating_max) {
+                if (rating > rating_max) {
                     rating_max_base = base_name;
                     rating_max = rating;
                 }
 
                 //기존 ArrayList에 RatingBase 객체 추가
-                HashMap<String,Integer> bMap = map.get(rating);
-                bMap.put(base_name, bMap.getOrDefault(base_name, 0)+ base_count);
+                HashMap<String, Integer> bMap = map.get(rating);
+                bMap.put(base_name, bMap.getOrDefault(base_name, 0) + base_count);
                 map.put(rating, bMap);
             }
 
-
-            for(int key : map.keySet()){
+            for (int key : map.keySet()) {
                 myAnalysisRatingBaseArrayList.add(map.get(key));
             }
 
-            rating_average = (double) total_rating/rating_count;
+            rating_average = (double) total_rating / rating_count;
             ratingBase = new RatingBase(rating_average, rating_count, rating_max, rating_max_base, myAnalysisRatingBaseArrayList);
 
             return ratingBase;
 
-        }catch (Exception e){
+        } catch (Exception e) {
             return ratingBase;
         }
     }
@@ -156,20 +155,20 @@ public class MyAnalysisUserServiceImpl implements MyAnalysisUserService {
         RatingColor ratingColor = new RatingColor();
         double rating_average = 0;
         int total_rating = 0;
-        int rating_count =0;
+        int rating_count = 0;
         int rating_max = Integer.MIN_VALUE;
         String rating_max_color = "";
 
-        try{
-            User user =  oAuthService.getUser(accessToken);
+        try {
+            User user = oAuthService.getUser(accessToken);
             ArrayList<MyAnalysisRatingColorInterface> interfaceArrayList = myAnalysisRepository.getMyAnalysisRatingColorList(user.getId());
             ArrayList<HashMap<String, Integer>> myAnalysisRatingColorArrayList = new ArrayList<>();
-            HashMap<Integer, HashMap<String,Integer>> map = new HashMap<>();
+            HashMap<Integer, HashMap<String, Integer>> map = new HashMap<>();
 
             for (int i = 1; i <= 5; i++) map.put(i, new RatingColorObj(i).getCMap());
 
             //데이터 정제 필요 ***
-            for(MyAnalysisRatingColorInterface ele : interfaceArrayList){
+            for (MyAnalysisRatingColorInterface ele : interfaceArrayList) {
                 int rating = ele.getRatingScore();
                 int color_count = ele.getColorCount();
 
@@ -192,19 +191,102 @@ public class MyAnalysisUserServiceImpl implements MyAnalysisUserService {
 
             }
 
-            for(int key : map.keySet()){
+            for (int key : map.keySet()) {
                 myAnalysisRatingColorArrayList.add(map.get(key));
             }
 
-            rating_average = (double) total_rating/rating_count;
+            rating_average = (double) total_rating / rating_count;
             ratingColor = new RatingColor(rating_average, rating_count, rating_max, rating_max_color, myAnalysisRatingColorArrayList);
 
             return ratingColor;
 
-        }catch (Exception e){
+        } catch (Exception e) {
             return ratingColor;
         }
     }
+
+    @Override
+    public RatingIngredient getAnalysisByRatingIngredient(String accessToken) {
+        RatingIngredient ratingIngredient = new RatingIngredient();
+        double rating_average = 0;
+        int total_rating = 0;
+        int rating_count = 0;
+        int rating_max = Integer.MIN_VALUE;
+        String rating_max_ingredient = "";
+
+        try {
+            User user = oAuthService.getUser(accessToken);
+            ArrayList<MyAnalysisRatingIngredientInterface> interfaceArrayList = myAnalysisRepository.getMyAnalysisRatingIngredientList(user.getId());
+            HashMap<Integer, ArrayList<RatingIngredientObj>> ingredientObjHashMap = new HashMap<>();
+            ArrayList<MyAnalysisRatingIngredient> myAnalysisRatingIngredients = new ArrayList<>();
+
+            // 해시맵 초기화
+            for (int i = 1; i <= 5; i++) ingredientObjHashMap.put(i, new ArrayList<>());
+
+            rating_count = interfaceArrayList.size();
+
+            for (MyAnalysisRatingIngredientInterface ele : interfaceArrayList) {
+                String ingredientName = ele.getIngredientName();
+                int ingredientCount = ele.getIngredientCount();
+                int ingredientRating = ele.getRatingScore();
+
+                total_rating += ingredientRating;
+
+                // 최대 평점으로 갱신
+                if (rating_max < ingredientRating) {
+                    rating_max = ingredientRating;
+                    rating_max_ingredient = ingredientName;
+                }
+
+                // 기존에 있는 리스트에 재료 객체 추가
+                ArrayList<RatingIngredientObj> addArr = ingredientObjHashMap.get(ingredientRating);
+                RatingIngredientObj obj = new RatingIngredientObj(ingredientName, ingredientCount);
+                addArr.add(obj);
+                ingredientObjHashMap.put(ingredientRating, addArr);
+            }
+
+            rating_average = (double) Math.round(total_rating / rating_count * 100) / 100.0;
+
+//          System.out.println(Arrays.toString(ingredientObjHashMap.get(2).toArray()));
+
+            for (Integer rating : ingredientObjHashMap.keySet()) {
+                    System.out.println(rating);
+                    ArrayList<String> names = new ArrayList<>();
+                    ArrayList<Integer> counts = new ArrayList<>();
+                    ArrayList<RatingIngredientObj> currArr = ingredientObjHashMap.get(rating);
+
+                    int size = currArr.size();
+
+                    for (int i = 0; i <5; i++) {
+                        if(i <size){
+                            RatingIngredientObj obj = currArr.get(i);
+                            names.add(i, obj.getIngredientName());
+                            counts.add(i, obj.getIngredientCount());
+
+                        }else{
+                            names.add(i, "");
+                            counts.add(i, 0);
+                        }
+                    }
+
+                    myAnalysisRatingIngredients.add(new MyAnalysisRatingIngredient(
+                            rating,
+                            names.get(0), counts.get(0),
+                            names.get(1), counts.get(1),
+                            names.get(2), counts.get(2),
+                            names.get(3), counts.get(3),
+                            names.get(4), counts.get(4)
+                    ));
+                }
+
+            ratingIngredient = new RatingIngredient(rating_average, rating_count, rating_max, rating_max_ingredient, myAnalysisRatingIngredients);
+
+            return ratingIngredient;
+        } catch (Exception e) {
+            return ratingIngredient;
+        }
+    }
+
 
     @Override
     public ArrayList<TestRecommend> getRecommendTest(String accessToken, String num) {
@@ -223,7 +305,7 @@ public class MyAnalysisUserServiceImpl implements MyAnalysisUserService {
         recommends.add(cocktailRepository.findCocktailById(recommendIngredient.getRecommendIngredient2())); // 추천 8번 삽입
         recommends.add(cocktailRepository.findCocktailById(recommendIngredient.getRecommendIngredient3())); // 추천 9번 삽입
 
-        for (Cocktail recommend: recommends) { // 추천 칵테일
+        for (Cocktail recommend : recommends) { // 추천 칵테일
             TestRecommend testRecommend = new TestRecommend();
             testRecommend.setCocktailId(recommend.getId()); // 칵테일 id 삽입
             testRecommend.setCocktailNameKo(recommend.getCocktailNameKo() + num); // 칵테일 한글 이름 삽입
