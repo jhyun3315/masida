b63b62cb052dd407d752f125dcf69e36d9430d7c
손종효 <jbson1998@naver.com>
2023-04-04 09:23:26 +0900

    fix : 검색페이지 검색 버그 수정 Issue : S08P22B208-670

--- frontend/src/components/Search/Result.tsx
+++ frontend/src/components/Search/Result.tsx
@@ -9,6 +9,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { useSelector } from "react-redux";
 import { RootState } from "../../../store/store";
+import { searchIngredientType } from "../../type/ingredientTypes";
 
 interface propsType {
   clickSearchBtn: boolean;
@@ -26,9 +27,6 @@ const Result: React.FunctionComponent<propsType> = ({ clickSearchBtn, addNumIngr
   let [rankChecked, setRankChecked] = useState<boolean>(false);
   let [sortCheckName, setSortCheckName] = useState<string>("");
 
-  //처음 시작할때, 검색할때 초기화 시켜줄 배열입니다.
-  const resetCocktail: cocktailType[] = [];
-
   let name = useSelector((state: RootState) => state.nameSelect.searchName);
   let base = useSelector((state: RootState) => state.baseSelect.base);
   let color = useSelector((state: RootState) => state.colorSelect.color);
@@ -39,13 +37,18 @@ const Result: React.FunctionComponent<propsType> = ({ clickSearchBtn, addNumIngr
     (state: RootState) => state.ingredientSelect.ingredient
   );
 
+  
+  //처음 시작할때, 검색할때 초기화 시켜줄 배열입니다.
+  const resetCocktail: cocktailType[] = [];
+  
+  
   const cocktailSearch = (sort: number) => {
     let tmpcolor: string = null;
     let tmpdifficulty: string = null;
     let tmpingredient: string = null;
     setCokctail(resetCocktail);
     setSortingNum(sort);
-
+    
     if (name === "") {
       name = null;
     }
@@ -63,22 +66,17 @@ const Result: React.FunctionComponent<propsType> = ({ clickSearchBtn, addNumIngr
       tmpdifficulty = difficulty.join(",");
     }
     //참인 것들만 재료에 담아줘..
-    ingredient.filter((ingre) =>
-      ingre.ingredient_add
-        ? setSendIngredient([...sendIngredient, ingre.ingredient_id])
-        : ""
-    );
     tmpingredient = addNumIngredient.join(",");
     
     setPage(0);
     console.log(name);
     
     axios
-      .get(`https://j8b208.p.ssafy.io/api/cocktails/search`, {
-        params: {
-          page: 0,
-          sort_num: sortingNum,
-          cocktail_name: name,
+    .get(`https://j8b208.p.ssafy.io/api/cocktails/search`, {
+      params: {
+        page: 0,
+        sort_num: sortingNum,
+        cocktail_name: name,
           cocktail_base: base,
           cocktail_color: tmpcolor,
           cocktail_difficulty: tmpdifficulty,
@@ -96,21 +94,21 @@ const Result: React.FunctionComponent<propsType> = ({ clickSearchBtn, addNumIngr
         //해당되는 데이터가 없다는 소리이므로 데이터의 총 개수 0으로 설정
         setCocktailCnt(0);
       });
-  };
-
-  useEffect(() => {
-    cocktailSearch(0);
-    setLikeChecked(false);
-    setRankChecked(false);
-  }, [clickSearchBtn]);
-
-  //정렬 기준이 바뀔때마다 useEffect 새로해주어야함.(즉 axios통신 새로해주기.)
-  useEffect(() => {
-    cocktailSearch(sortingNum);
-  }, [sortingNum]);
-
-  //현재 선택한 것들을 기준으로 좋아요 순으로 정렬.
-  const sortLike = (e : React.MouseEvent<HTMLLabelElement>) => {
+    };
+    
+    useEffect(() => {
+      cocktailSearch(0);
+      setLikeChecked(false);
+      setRankChecked(false);
+    }, [clickSearchBtn]);
+    
+    //정렬 기준이 바뀔때마다 useEffect 새로해주어야함.(즉 axios통신 새로해주기.)
+    useEffect(() => {
+      cocktailSearch(sortingNum);
+    }, [sortingNum]);
+    
+    //현재 선택한 것들을 기준으로 좋아요 순으로 정렬.
+    const sortLike = (e : React.MouseEvent<HTMLLabelElement>) => {
     const target = e.target as HTMLLabelElement;
     if(sortCheckName === target.htmlFor) {
       setLikeChecked(false);
@@ -124,7 +122,7 @@ const Result: React.FunctionComponent<propsType> = ({ clickSearchBtn, addNumIngr
       setSortingNum(1);
     }
   };
-
+  
   //현재 선택한 것들을 기준으로 별점 순으로 정렬
   const sortRating = (e : React.MouseEvent<HTMLLabelElement>) => {
     const target = e.target as HTMLLabelElement;
@@ -141,43 +139,50 @@ const Result: React.FunctionComponent<propsType> = ({ clickSearchBtn, addNumIngr
       setSortingNum(2);
     }
   };
+  
+  //무한 스크롤을 위한 베이스, 색상, 난이도, 재료가 있어야 할것 같다.(useEffect로 처음 실행할때만 동작하게.)
+  const [saveName, setSaveName] = useState<string>();
+  const [saveBase, setSaveBase] = useState<string>();
+  const [saveColor, setSaveColor] = useState<string[]>([]);
+  const [saveDifficulty, setSaveDifficulty] = useState<string[]>([]);
+  const [saveIngredient, setSaveIngredient] = useState<searchIngredientType[]>([]);
+
+  useEffect(() => {
+    setSaveName(name);
+    setSaveBase(base);
+    setSaveColor(color);
+    setSaveDifficulty(difficulty);
+    setSaveIngredient(ingredient);
+  }, [])
 
   //무한 스크롤 구현.
   const handelScroll = (e: React.UIEvent<HTMLDivElement>) => {
     const target = e.target as HTMLDivElement;
     const isEnd =
-      Math.round(target.scrollTop + target.clientHeight) >
+    Math.round(target.scrollTop + target.clientHeight) >
       target.scrollHeight - 20;
-
+      
     if (isEnd && !pageEnd) {
       let tmpcolor: string = null;
       let tmpdifficulty: string = null;
       let tmpingredient: string = null;
       
 
-      if (base === "") {
-        base = null;
+      if (saveBase === "") {
+        setSaveBase(null);
       }
-      if (color?.length === 0) {
+      if (saveColor?.length === 0) {
         tmpcolor = null;
       } else {
-        tmpcolor = color.join(",");
+        tmpcolor = saveColor.join(",");
       }
-      if (difficulty?.length === 0) {
+      if (saveDifficulty?.length === 0) {
         tmpdifficulty = null;
       } else {
-        tmpdifficulty = difficulty.join(",");
+        tmpdifficulty = saveDifficulty.join(",");
       }
       //참인 것들만 재료에 담아줘..
-      ingredient.filter((ingre) =>
-        ingre.ingredient_add
-          ? setSendIngredient([...sendIngredient, ingre.ingredient_id])
-          : ""
-      );
-      tmpingredient = sendIngredient.join(",");
-
-      console.log(name);
-      
+      tmpingredient = saveIngredient.join(",");
 
       //끝을 감지했다면?
       axios
@@ -185,8 +190,8 @@ const Result: React.FunctionComponent<propsType> = ({ clickSearchBtn, addNumIngr
           params: {
             page: page,
             sort_num: sortingNum,
-            cocktail_name: name,
-            cocktail_base: base,
+            cocktail_name: saveName,
+            cocktail_base: saveBase,
             cocktail_color: tmpcolor,
             cocktail_difficulty: tmpdifficulty,
             cocktail_ingredient: tmpingredient,
