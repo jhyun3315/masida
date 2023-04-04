f165a3a03fb9970752cffe19bd42ee37726c12ac
juseong <kjschocolate@gmail.com>
2023-04-04 12:59:02 +0900

    refactor: Cocktail dto 리팩토링

--- backend/src/main/java/com/ssafy/cocktail/backend/cocktails/dto/SearchInfo.java
+++ backend/src/main/java/com/ssafy/cocktail/backend/cocktails/dto/SearchInfo.java
@@ -1,6 +1,5 @@
 package com.ssafy.cocktail.backend.cocktails.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
