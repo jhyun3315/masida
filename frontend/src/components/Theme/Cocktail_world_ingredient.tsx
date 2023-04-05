30d88a3a65753c13053af8482ec8d741cf9852ff
김영주 <eorhrtnv@naver.com>
2023-04-05 16:55:16 +0900

    fix user Analisys

--- frontend/src/components/Theme/Cocktail_world_ingredient.tsx
+++ frontend/src/components/Theme/Cocktail_world_ingredient.tsx
@@ -7,13 +7,10 @@ import { cocktail_world_ingredientType } from '../../pages/theme/cocktail_world'
 const Cocktail_world_ingredient = (props: cocktail_world_ingredientType) => {
   const isLoading = props.isLoading;
   const wordList = props.wordList;
-  
-  console.log(wordList,"아 시발")
-  // const word:wordType[]= [];
-  // for (let i = 0; i < 50; i++){
-  //   word.push({text:wordList[i].text, value:wordList[i].value});
-  // }
 
+  let arr: any = [];
+  arr = [...arr, wordList]; 
+  
  type Options = {
   rotations?: number;
   rotationAngles?: MinMaxPair;
@@ -24,17 +21,17 @@ const options: Options = {
   rotationAngles: [0, 360] ,
   }
 
-  const size: MinMaxPair = [1400, 400];
+  const size: MinMaxPair = [1250, 400];
   if (isLoading) { 
      return (
     <div className={ style.cocktailWord_ingredient}>
       <h1>Most popular Ingredients</h1>
       <p>3700개의 칵테일에서 쓰인 재료 TOP 50</p>
-      {/* <ReactWordcloud
-        words={word}
+      <ReactWordcloud
+        words={arr[0]}
         options={options}
         size={size}
-      /> */}
+      />
     </div>
   )
   }
--- frontend/src/type/cocktailWord.tsx
+++ frontend/src/type/cocktailWord.tsx
@@ -1,7 +1,8 @@
 // 칵테일 월드 단어 데이터
 export type wordType = {
   text: string,
-  value:number
+  value: number,
+  length: number
 }
 
 // 칵테일 월드 데이터
