76b5a0814386955f1b83fe790ec7b5ab4788d76b
juseong <kjschocolate@gmail.com>
2023-04-05 22:45:43 +0900

    feat: 칵테일 사용자 분석 색상 추천 관련 JAP 추가

--- backend/src/main/java/com/ssafy/cocktail/backend/domain/repository/CocktailRepository.java
+++ backend/src/main/java/com/ssafy/cocktail/backend/domain/repository/CocktailRepository.java
@@ -3,6 +3,7 @@ package com.ssafy.cocktail.backend.domain.repository;
 import com.ssafy.cocktail.backend.cocktails.dto.CocktailLikesInterface;
 import com.ssafy.cocktail.backend.domain.entity.Cocktail;
 import com.ssafy.cocktail.backend.myAnalysis.dto.MyAnalysisBase;
+import com.ssafy.cocktail.backend.myAnalysis.dto.MyAnalysisRecommendInterface;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -32,6 +33,34 @@ public interface CocktailRepository extends JpaRepository<Cocktail, Long> {
     CocktailLikesInterface getCocktailRandomOne();
 
     Cocktail findCocktailById(Long cocktailId);
+
+    @Query(value = "select c.cocktail_id as cocktailId " +
+            ", c.cocktail_name_ko as cocktailNameKo " +
+            ", c.cocktail_img as cocktailImg " +
+            ", count(c.cocktail_id) as cnt " +
+            "from cocktails c " +
+            "join likes l on c.cocktail_id = l.cocktail_id " +
+            "where c.cocktail_color1 = :color " +
+            "group by cocktailId " +
+            "order by 'cnt' desc limit 3", nativeQuery = true)
+    List<MyAnalysisRecommendInterface> getCocktailByColorAndLikesCnt(@Param("color") String color);
+
+    @Query(value = "select cocktail_id as cocktailId " +
+            ", cocktail_name_ko as cocktailNameKo " +
+            ", cocktail_img as cocktailImg " +
+            "from cocktails " +
+            "where cocktail_color1 = :color " +
+            "order by rand() limit 3", nativeQuery = true)
+    List<MyAnalysisRecommendInterface> getCocktailByColorAndRandom(@Param("color") String color);
+
+    @Query(value = "select cocktail_id as cocktailId " +
+            ", cocktail_name_ko as cocktailNameKo " +
+            ", cocktail_name_en as cocktailNameEn " +
+            ", cocktail_img as cocktailImg " +
+            ", cocktail_rating as cocktailRating " +
+            "from cocktails " +
+            "order by rand() limit 3", nativeQuery = true)
+    List<MyAnalysisRecommendInterface> getCocktailByRandomCnt();
 
 
 }
