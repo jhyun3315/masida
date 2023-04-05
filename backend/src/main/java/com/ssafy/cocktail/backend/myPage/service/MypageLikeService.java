bd11099a3fe3e51b377fdeccd01d22f96c593af0
이지현 <jiannalee3@gmail.com>
2023-04-05 10:37:41 +0900

    Revert "Merge branch 'backend'"
    This reverts commit 83d602f85ff5fe7af9160a6427591eab335cbd29

--- backend/src/main/java/com/ssafy/cocktail/backend/myPage/service/MypageLikeService.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.cocktail.backend.myPage.service;
-
-import com.ssafy.cocktail.backend.myPage.dto.PaginationDataSet;
-import org.springframework.data.domain.Pageable;
-
-public interface MypageLikeService {
-	public PaginationDataSet getLikeCocktailList(Long userId, Pageable pageable);
-	public Long getLikeCocktailCnt(Long userId);
-}
