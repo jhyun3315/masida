313183b0df1904fffbdea3fe9e198c5359651505
juseong <kjschocolate@gmail.com>
2023-04-06 10:06:29 +0900

    refacter: 칵테일 월드컵 리팩토링

--- backend/src/main/java/com/ssafy/cocktail/backend/worldcups/service/impl/WorldCupImpl.java
+++ backend/src/main/java/com/ssafy/cocktail/backend/worldcups/service/impl/WorldCupImpl.java
@@ -25,6 +25,7 @@ public class WorldCupImpl implements WorldCupService {
     private CocktailIngredientRepository cocktailIngredientRepository;
     private LikeRepository likeRepository;
     private CommentRepository commentRepository;
+
     @Override
     public ArrayList<CocktailWorldCupDetail> getWorldCupCocktails() {
         // 랜덤으로 16개 칵테일을 뽑아 리턴
@@ -32,31 +33,31 @@ public class WorldCupImpl implements WorldCupService {
         List<Cocktail> cocktails = cocktailRepository.findAll(); // 모든 칵테일 가져오기
         Collections.shuffle(cocktails); // 랜덤으로 칵테일 정렬
         for (int i = 0; i < 16; i++) {
-            Cocktail cocktail = cocktails.get(i);
-            CocktailWorldCupDetail cand = new CocktailWorldCupDetail(); // 월드컵에 넣을 칵테일
-            cand.setCocktailId(cocktail.getId()); // 칵테일 id 삽입
-            cand.setCocktailNameKo(cocktail.getCocktailNameKo()); // 칵테일 한글 이름 삽입
-            cand.setCocktailNameEn(cocktail.getCocktailNameEn()); // 칵테일 영어 이름 삽입
-            cand.setCocktailImg(cocktail.getCocktailImg()); // 칵테일 이미지 삽입
-            cand.setCocktailContent(cocktail.getCocktailContent()); // 칵테일 내용 삽입
-            cand.setIngredient(new ArrayList<IngredientInfo>()); // 칵테일 재료를 저장하는 객체 삽입
+            Cocktail cocktail = cocktails.get(i); // 칵테일 가져오기
+            CocktailWorldCupDetail worldCocktail = new CocktailWorldCupDetail(); // 월드컵에 넣을 칵테일
+            worldCocktail.setCocktailId(cocktail.getId()); // 칵테일 id 삽입
+            worldCocktail.setCocktailNameKo(cocktail.getCocktailNameKo()); // 칵테일 한글 이름 삽입
+            worldCocktail.setCocktailNameEn(cocktail.getCocktailNameEn()); // 칵테일 영어 이름 삽입
+            worldCocktail.setCocktailImg(cocktail.getCocktailImg()); // 칵테일 이미지 삽입
+            worldCocktail.setCocktailContent(cocktail.getCocktailContent()); // 칵테일 내용 삽입
+            worldCocktail.setIngredient(new ArrayList<IngredientInfo>()); // 칵테일 재료를 저장하는 객체 삽입
             String cocktaildifficulty = (int)cocktail.getCocktailDifficulty() == 1 ? "하" :
-                    cocktail.getCocktailDifficulty() == 2 ? "중" : "상"; // 칵테일 난이도
-            cand.setCocktailDifficulty(cocktaildifficulty); // 칵테일 난이도 삽입
-            cand.setCocktailRating(Math.round(cocktail.getCocktailRating() * 10.0) / 10.0); // 칵테일 평점 삽입
+                    (int)cocktail.getCocktailDifficulty() == 2 ? "중" : "상"; // 칵테일 난이도
+            worldCocktail.setCocktailDifficulty(cocktaildifficulty); // 칵테일 난이도 삽입
+            worldCocktail.setCocktailRating(Math.round(cocktail.getCocktailRating() * 10.0) / 10.0); // 칵테일 평점 삽입
             List<Like> likes = likeRepository.findAllByCocktailAndLikeDeleted(cocktail, false); // 좋아요 가져오기
-            cand.setCocktailLikes(likes.size()); // 좋아요 개수 삽입
+            worldCocktail.setCocktailLikes(likes.size()); // 좋아요 개수 삽입
             List<Comment> comments = commentRepository.findAllByCocktailAndCommentDeleted(cocktail, false); // 댓글 가져오기
-            cand.setCocktailComments(comments.size()); // 댓글 개수 삽입
+            worldCocktail.setCocktailComments(comments.size()); // 댓글 개수 삽입
             List<CocktailIngredient> cocktailIngredients = cocktailIngredientRepository.findByCocktail(cocktail); // 칵테일 재료들
             for (CocktailIngredient cocktailIngredient: cocktailIngredients) { // 칵테일 재료
                 IngredientInfo ingredientInfo = new IngredientInfo();
                 ingredientInfo.setIngredientName(cocktailIngredient.getIngredientName()); // 재료 이름 삽입
                 ingredientInfo.setIngredientAmount(cocktailIngredient.getIngredientAmount()); // 재료 양 삽입
                 ingredientInfo.setIngredientUnit(cocktailIngredient.getIngredientUnit()); // 재료 단위 삽입
-                cand.getIngredient().add(ingredientInfo); // 재료 삽입
+                worldCocktail.getIngredient().add(ingredientInfo); // 재료 삽입
             }
-            cocktailWorldCupDetails.add(cand); // 월드컵 칵테일 추가
+            cocktailWorldCupDetails.add(worldCocktail); // 월드컵 칵테일 추가
         }
         return cocktailWorldCupDetails;
     }
