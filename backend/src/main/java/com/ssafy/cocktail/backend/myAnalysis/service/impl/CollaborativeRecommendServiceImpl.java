1ae8cb5745b90a89ac3f8211e75a64b6590f26a4
jis22u <jis22u@gmail.com>
2023-04-06 17:37:22 +0900

    fix:파이썬 콜 수정

--- backend/src/main/java/com/ssafy/cocktail/backend/myAnalysis/service/impl/CollaborativeRecommendServiceImpl.java
+++ backend/src/main/java/com/ssafy/cocktail/backend/myAnalysis/service/impl/CollaborativeRecommendServiceImpl.java
@@ -26,7 +26,7 @@ import java.util.*;
 @Transactional
 @AllArgsConstructor
 public class CollaborativeRecommendServiceImpl implements CollaborativeRecommendService {
-	private final String PYTHON_API_URL = "http://localhost:8000/py";
+	private final String PYTHON_API_URL = "https://j8b208.p.ssafy.io:8000";
 	private MyAnalysisRepository myAnalysisRepository;
 	private LikeRepository likeRepository;
 	private CocktailRepository cocktailRepository;
